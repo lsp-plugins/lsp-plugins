1c749a146cfa96abcf50682a0797e2161020a752
SadKo <lsp.plugin@gmail.com>
2018-08-19 00:23:18 +0300

    Added performance test filtering possibility

--- main.cpp
+++ main.cpp
@@ -148,13 +148,119 @@
 //#define TEST selection_test
 //#define TEST endian_test
 
-#include <dsp/dsp.h>
-#include <test/ptest.h>
 #include <errno.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
-int launch_ptest()
+#include <dsp/dsp.h>
+#include <test/ptest.h>
+#include <data/cvector.h>
+#include <core/LSPString.h>
+
+enum test_mode_t
+{
+    UNKNOWN,
+    PTEST
+};
+
+using namespace lsp;
+
+typedef struct config_t
+{
+    mode_t                      mode;
+    bool                        fork;
+    cvector<LSPString>          list;
+} config_t;
+
+bool match_string(const LSPString *p, const LSPString *m)
+{
+    size_t m_off = 0;
+
+    for (size_t off = 0, len = p->length(); off < len; )
+    {
+        // Get pattern token
+        LSPString *pt = NULL;
+        ssize_t next = p->index_of(off, '.');
+        if (next < 0)
+        {
+            pt = p->substring(off);
+            off = p->length();
+        }
+        else
+        {
+            pt = p->substring(off, next);
+            off = next + 1;
+        }
+
+        // Get match token
+        if (m_off >= m->length())
+            return false;
+
+        LSPString *mt = NULL;
+        next = m->index_of(m_off, '.');
+        if (next < 0)
+        {
+            mt = m->substring(m_off);
+            m_off = m->length();
+        }
+        else
+        {
+            mt = m->substring(m_off, next);
+            m_off = next + 1;
+        }
+
+        // Check wildcard
+        if ((pt->length() == 1) && (pt->char_at(0) == '*'))
+            continue;
+
+        // Check equality
+        if (!pt->equals(mt))
+            return false;
+    }
+
+    return true;
+}
+
+bool match_list(lsp::cvector<LSPString> &list, const char *group, const char *name)
+{
+    // Empty list always matches
+    if (list.size() <= 0)
+        return true;
+
+    LSPString m;
+    m.set_ascii(group);
+    m.append('.');
+    m.append_ascii(name);
+
+    for (size_t i=0, n=list.size(); i<n; ++i)
+    {
+        LSPString *p = list.at(i);
+        if (p == NULL)
+            continue;
+        if (match_string(p, &m))
+            return true;
+    }
+
+    return false;
+}
+
+int execute_ptest(test::ptest::PerformanceTest *v)
+{
+    srand(clock());
+
+    // Nested process code: initialize DSP
+    dsp::context_t ctx;
+    dsp::init();
+    dsp::start(&ctx);
+
+    // Execute performance test
+    v->execute();
+
+    dsp::finish(&ctx);
+    return 0;
+}
+
+int launch_ptest(config_t *cfg)
 {
     using namespace test::ptest;
     PerformanceTest *v = init();
@@ -173,62 +279,62 @@ int launch_ptest()
 
     for ( ; v != NULL; v = v->next())
     {
+        // Need to check test name and group?
+        if (!match_list(cfg->list, v->group(), v->name()))
+            continue;
+
         printf("--------------------------------------------------------------------------------\n");
         printf("Launching performance test '%s' group '%s'\n", v->name(), v->group());
         printf("--------------------------------------------------------------------------------\n");
 
         clock_gettime(CLOCK_REALTIME, &start);
 
-        pid_t pid = fork();
-        if (pid < 0) {
-            int error = errno;
-            fprintf(stderr, "Error while spawning child process %d\n", error);
+        if (cfg->fork)
+        {
+            pid_t pid = fork();
+            if (pid < 0) {
+                int error = errno;
+                fprintf(stderr, "Error while spawning child process %d\n", error);
 
-            result = -2;
-            break;
-        } else if (pid == 0) {
-            srand(clock());
-
-            // Nested process code: initialize DSP
-            dsp::context_t ctx;
-            dsp::init();
-            dsp::start(&ctx);
-
-            // Execute performance test
-            v->execute();
-
-            dsp::finish(&ctx);
-            return 0;
-        } else {
-            // Parent process code: wait for nested process execution
-            total ++;
-            do
-            {
-                int w = waitpid(pid, &result, WUNTRACED | WCONTINUED);
-                if (w < 0)
+                result = -2;
+                break;
+            } else if (pid == 0) {
+                return execute_ptest(v);
+            } else {
+                // Parent process code: wait for nested process execution
+                total ++;
+                do
                 {
-                    fprintf(stderr, "Waiting for performance test '%s' group '%s failed\n", v->name(), v->group());
-                    failed++;
-                    break;
-                }
+                    int w = waitpid(pid, &result, WUNTRACED | WCONTINUED);
+                    if (w < 0)
+                    {
+                        fprintf(stderr, "Waiting for performance test '%s' group '%s failed\n", v->name(), v->group());
+                        failed++;
+                        break;
+                    }
 
-                if (WIFEXITED(result))
-                    printf("Performance test '%s' group '%s' finished, status=%d\n", v->name(), v->group(), WEXITSTATUS(result));
-                else if (WIFSIGNALED(result))
-                    printf("Performance test '%s' group '%s' killed by signal %d\n", v->name(), v->group(), WTERMSIG(result));
-                else if (WIFSTOPPED(result))
-                    printf("Performance test '%s' group '%s' stopped by signal %d\n", v->name(), v->group(), WSTOPSIG(result));
-            } while (!WIFEXITED(result) && !WIFSIGNALED(result));
-
-            clock_gettime(CLOCK_REALTIME, &finish);
-            time = (finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec) * 1e-9;
-
-            printf("Test execution time: %.2f s\n", time);
-            if (result == 0)
-                success ++;
-            else
-                failed ++;
+                    if (WIFEXITED(result))
+                        printf("Performance test '%s' group '%s' finished, status=%d\n", v->name(), v->group(), WEXITSTATUS(result));
+                    else if (WIFSIGNALED(result))
+                        printf("Performance test '%s' group '%s' killed by signal %d\n", v->name(), v->group(), WTERMSIG(result));
+                    else if (WIFSTOPPED(result))
+                        printf("Performance test '%s' group '%s' stopped by signal %d\n", v->name(), v->group(), WSTOPSIG(result));
+                } while (!WIFEXITED(result) && !WIFSIGNALED(result));
+            }
         }
+        else
+        {
+            total ++;
+            result = execute_ptest(v);
+        }
+        clock_gettime(CLOCK_REALTIME, &finish);
+        time = (finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec) * 1e-9;
+
+        printf("Test execution time: %.2f s\n", time);
+        if (result == 0)
+            success ++;
+        else
+            failed ++;
     }
 
     time = (finish.tv_sec - ts.tv_sec) + (finish.tv_nsec - ts.tv_nsec) * 1e-9;
@@ -242,11 +348,7 @@ int launch_ptest()
     return (failed > 0) ? 0 : 2;
 }
 
-enum test_mode_t
-{
-    UNKNOWN,
-    PTEST
-};
+
 
 int usage()
 {
@@ -254,27 +356,60 @@ int usage()
     return 1;
 }
 
-int main(int argc, const char **argv)
+int parse_config(config_t *cfg, int argc, const char **argv)
 {
-    test_mode_t mode = UNKNOWN;
+    cfg->mode   = UNKNOWN;
+    cfg->fork   = true;
     if (argc < 2)
         return usage();
 
     if (!strcmp(argv[1], "ptest"))
-        mode = PTEST;
-    if (mode == UNKNOWN)
+        cfg->mode = PTEST;
+    if (cfg->mode == UNKNOWN)
         return usage();
 
+    for (int i=2; i<argc; ++i)
+    {
+        if (!strcmp(argv[i], "--nofork"))
+            cfg->fork       = false;
+        else if (!strcmp(argv[i], "--fork"))
+            cfg->fork       = true;
+        else
+        {
+            LSPString *s = new LSPString();
+            s->set_native(argv[i]);
+            cfg->list.add(s);
+        }
+    }
+
+    return 0;
+}
+
+void clear_config(config_t *cfg)
+{
+    for (size_t i=0, n=cfg->list.size(); i<n; ++i)
+        delete cfg->list.at(i);
+    cfg->list.clear();
+}
+
+int main(int argc, const char **argv)
+{
+    config_t cfg;
+    int res = parse_config(&cfg, argc, argv);
+    if (res != 0)
+        return res;
+
     // Launch tests
-    int res = 0;
-    switch (mode)
+    switch (cfg.mode)
     {
         case PTEST:
-            res = launch_ptest();
+            res = launch_ptest(&cfg);
             break;
         default:
             break;
     }
+
+    clear_config(&cfg);
 
     return res;
     /*srand(clock());
