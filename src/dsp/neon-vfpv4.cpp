d601db861acb6a4c3c5cc17fbd95f32f3058964c
SadKo <lsp.plugin@gmail.com>
2018-09-02 02:53:33 +0300

    Added prototype for unpacked complex multiplication using ARM NEON

---
+++ src/dsp/neon-vfpv4.cpp
@@ -0,0 +1,13 @@
+/*
+ * neon-vfpv4.cpp
+ *
+ *  Created on: 2 сент. 2018 г.
+ *      Author: sadko
+ */
+
+
+#include <dsp/dsp.h>
+
+#ifdef ARCH_ARM
+    #include <dsp/arch/arm/neon-vfpv4/complex.h>
+#endif /* ARCH_ARM */
