8398870072579ef57ae07d45e6854614a299d821
SadKo <lsp.plugin@gmail.com>
2018-09-06 21:48:23 +0300

    Unit test refactoring

--- src/test/utest/dsp/complex/mul.cpp
+++ src/test/utest/dsp/complex/mul.cpp
@@ -12,33 +12,19 @@
 namespace native
 {
     void complex_mul(float *dst_re, float *dst_im, const float *src1_re, const float *src1_im, const float *src2_re, const float *src2_im, size_t count);
-    void pcomplex_mul(float *dst, const float *src1, const float *src2, size_t count);
 }
 
 IF_ARCH_X86(
     namespace sse
     {
         void complex_mul(float *dst_re, float *dst_im, const float *src1_re, const float *src1_im, const float *src2_re, const float *src2_im, size_t count);
-        void pcomplex_mul(float *dst, const float *src1, const float *src2, size_t count);
-    }
-
-    namespace sse3
-    {
-        void pcomplex_mul(float *dst, const float *src1, const float *src2, size_t count);
     }
 
     IF_ARCH_X86_64(
-        namespace sse3
-        {
-            void x64_pcomplex_mul(float *dst, const float *src1, const float *src2, size_t count);
-        }
-
         namespace avx
         {
             void x64_complex_mul(float *dst_re, float *dst_im, const float *src1_re, const float *src1_im, const float *src2_re, const float *src2_im, size_t count);
             void x64_complex_mul_fma3(float *dst_re, float *dst_im, const float *src1_re, const float *src1_im, const float *src2_re, const float *src2_im, size_t count);
-            void x64_pcomplex_mul(float *dst, const float *src1, const float *src2, size_t count);
-            void x64_pcomplex_mul_fma3(float *dst, const float *src1, const float *src2, size_t count);
         }
     )
 )
@@ -48,55 +34,13 @@ IF_ARCH_ARM(
     {
         void complex_mul3(float *dst_re, float *dst_im, const float *src1_re, const float *src1_im, const float *src2_re, const float *src2_im, size_t count);
         void complex_mul3_x12(float *dst_re, float *dst_im, const float *src1_re, const float *src1_im, const float *src2_re, const float *src2_im, size_t count);
-
-        void pcomplex_mul3(float *dst, const float *src1, const float *src2, size_t count);
     }
 )
 
 typedef void (* complex_mul_t) (float *dst_re, float *dst_im, const float *src1_re, const float *src1_im, const float *src2_re, const float *src2_im, size_t count);
-typedef void (* packed_complex_mul_t) (float *dst, const float *src1, const float *src2, size_t count);
 
 
 UTEST_BEGIN("dsp.complex", mul)
-
-    void call(const char *text,  size_t align, packed_complex_mul_t func)
-    {
-        if (!UTEST_SUPPORTED(func))
-            return;
-
-        UTEST_FOREACH(count, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-                32, 33, 37, 48, 49, 64, 65, 0x3f, 100, 999, 0x1fff)
-        {
-            for (size_t mask=0; mask <= 0x07; ++mask)
-            {
-                printf("Testing %s on input buffer of %d numbers, mask=0x%x...\n", text, int(count), int(mask));
-
-                FloatBuffer src1(count*2, align, mask & 0x01);
-                FloatBuffer src2(count*2, align, mask & 0x02);
-                FloatBuffer dst1(count*2, align, mask & 0x04);
-                FloatBuffer dst2(count*2, align, mask & 0x04);
-
-                // Call functions
-                native::pcomplex_mul(dst1, src1, src2, count);
-                func(dst2, src1, src2, count);
-
-                UTEST_ASSERT_MSG(src1.valid(), "Source buffer 1 corrupted");
-                UTEST_ASSERT_MSG(src2.valid(), "Source buffer 2 corrupted");
-                UTEST_ASSERT_MSG(dst1.valid(), "Destination buffer 1 corrupted");
-                UTEST_ASSERT_MSG(dst2.valid(), "Destination buffer 2 corrupted");
-
-                // Compare buffers
-                if (!dst1.equals_absolute(dst2, 1e-5))
-                {
-                    src1.dump("src1");
-                    src2.dump("src2");
-                    dst1.dump("dst1");
-                    dst2.dump("dst2");
-                    UTEST_FAIL_MSG("Output of functions for test '%s' differs", text);
-                }
-            }
-        }
-    }
 
     void call(const char *text,  size_t align, complex_mul_t func)
     {
@@ -151,18 +95,11 @@ UTEST_BEGIN("dsp.complex", mul)
 
     UTEST_MAIN
     {
-        IF_ARCH_X86(call("unpacked_sse", 16, sse::complex_mul));
-        IF_ARCH_X86_64(call("x64_unpacked_avx", 16, avx::x64_complex_mul));
-        IF_ARCH_X86_64(call("x64_unpacked_fma3", 16, avx::x64_complex_mul_fma3));
-        IF_ARCH_ARM(call("unpacked_neon_d32", 16, neon_d32::complex_mul3));
-        IF_ARCH_ARM(call("unpacked_neon_d32_x12", 16, neon_d32::complex_mul3_x12));
-
-        IF_ARCH_X86(call("packed_sse", 16, sse::pcomplex_mul));
-        IF_ARCH_X86(call("packed_sse3", 16, sse3::pcomplex_mul));
-        IF_ARCH_X86_64(call("x64_packed_sse3", 16, sse3::x64_pcomplex_mul));
-        IF_ARCH_X86_64(call("x64_packed_avx", 32, avx::x64_pcomplex_mul));
-        IF_ARCH_X86_64(call("x64_packed_fma3", 32, avx::x64_pcomplex_mul_fma3));
-        IF_ARCH_ARM(call("packed_neon_d32", 16, neon_d32::pcomplex_mul3));
+        IF_ARCH_X86(call("sse:complex_mul", 16, sse::complex_mul));
+        IF_ARCH_X86_64(call("avx:x64_complex_mul", 16, avx::x64_complex_mul));
+        IF_ARCH_X86_64(call("fma3:x64_complex_mul", 16, avx::x64_complex_mul_fma3));
+        IF_ARCH_ARM(call("neon_d32:complex_mul", 16, neon_d32::complex_mul3));
+        IF_ARCH_ARM(call("neon_d32:complex_mul_x12", 16, neon_d32::complex_mul3_x12));
     }
 
 UTEST_END;
---
+++ src/test/utest/dsp/pcomplex/mul.cpp
@@ -0,0 +1,106 @@
+/*
+ * mul.cpp
+ *
+ *  Created on: 23 авг. 2018 г.
+ *      Author: sadko
+ */
+
+#include <dsp/dsp.h>
+#include <test/utest.h>
+#include <test/FloatBuffer.h>
+
+namespace native
+{
+    void pcomplex_mul(float *dst, const float *src1, const float *src2, size_t count);
+}
+
+IF_ARCH_X86(
+    namespace sse
+    {
+        void pcomplex_mul(float *dst, const float *src1, const float *src2, size_t count);
+    }
+
+    namespace sse3
+    {
+        void pcomplex_mul(float *dst, const float *src1, const float *src2, size_t count);
+    }
+
+    IF_ARCH_X86_64(
+        namespace sse3
+        {
+            void x64_pcomplex_mul(float *dst, const float *src1, const float *src2, size_t count);
+        }
+
+        namespace avx
+        {
+            void x64_pcomplex_mul(float *dst, const float *src1, const float *src2, size_t count);
+            void x64_pcomplex_mul_fma3(float *dst, const float *src1, const float *src2, size_t count);
+        }
+    )
+)
+
+IF_ARCH_ARM(
+    namespace neon_d32
+    {
+        void complex_mul3(float *dst_re, float *dst_im, const float *src1_re, const float *src1_im, const float *src2_re, const float *src2_im, size_t count);
+        void complex_mul3_x12(float *dst_re, float *dst_im, const float *src1_re, const float *src1_im, const float *src2_re, const float *src2_im, size_t count);
+
+        void pcomplex_mul3(float *dst, const float *src1, const float *src2, size_t count);
+    }
+)
+
+typedef void (* packed_complex_mul_t) (float *dst, const float *src1, const float *src2, size_t count);
+
+
+UTEST_BEGIN("dsp.pcomplex", mul)
+
+    void call(const char *text,  size_t align, packed_complex_mul_t func)
+    {
+        if (!UTEST_SUPPORTED(func))
+            return;
+
+        UTEST_FOREACH(count, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+                32, 33, 37, 48, 49, 64, 65, 0x3f, 100, 999, 0x1fff)
+        {
+            for (size_t mask=0; mask <= 0x07; ++mask)
+            {
+                printf("Testing %s on input buffer of %d numbers, mask=0x%x...\n", text, int(count), int(mask));
+
+                FloatBuffer src1(count*2, align, mask & 0x01);
+                FloatBuffer src2(count*2, align, mask & 0x02);
+                FloatBuffer dst1(count*2, align, mask & 0x04);
+                FloatBuffer dst2(count*2, align, mask & 0x04);
+
+                // Call functions
+                native::pcomplex_mul(dst1, src1, src2, count);
+                func(dst2, src1, src2, count);
+
+                UTEST_ASSERT_MSG(src1.valid(), "Source buffer 1 corrupted");
+                UTEST_ASSERT_MSG(src2.valid(), "Source buffer 2 corrupted");
+                UTEST_ASSERT_MSG(dst1.valid(), "Destination buffer 1 corrupted");
+                UTEST_ASSERT_MSG(dst2.valid(), "Destination buffer 2 corrupted");
+
+                // Compare buffers
+                if (!dst1.equals_absolute(dst2, 1e-5))
+                {
+                    src1.dump("src1");
+                    src2.dump("src2");
+                    dst1.dump("dst1");
+                    dst2.dump("dst2");
+                    UTEST_FAIL_MSG("Output of functions for test '%s' differs", text);
+                }
+            }
+        }
+    }
+
+    UTEST_MAIN
+    {
+        IF_ARCH_X86(call("sse:pcomplex_mul", 16, sse::pcomplex_mul));
+        IF_ARCH_X86(call("sse3:pcomplex_mul", 16, sse3::pcomplex_mul));
+        IF_ARCH_X86_64(call("sse3:x64_pcomplex_mul", 16, sse3::x64_pcomplex_mul));
+        IF_ARCH_X86_64(call("avx:x64_pcomplex_mul", 32, avx::x64_pcomplex_mul));
+        IF_ARCH_X86_64(call("fma3:x64_pcomplex_mul", 32, avx::x64_pcomplex_mul_fma3));
+        IF_ARCH_ARM(call("neon_d32:pcomplex_mul", 16, neon_d32::pcomplex_mul3));
+    }
+
+UTEST_END;
