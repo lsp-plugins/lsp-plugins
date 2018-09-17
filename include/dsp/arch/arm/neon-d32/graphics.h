2ac51570191b1732a2cc553dcb74cddd1cbf853f
SadKo <lsp.plugin@gmail.com>
2018-09-17 14:21:52 +0300

    Fixed neon_d32::rgba32_to_bgra32 routine.

--- include/dsp/arch/arm/neon-d32/graphics.h
+++ include/dsp/arch/arm/neon-d32/graphics.h
@@ -33,7 +33,7 @@ namespace neon_d32
             __ASM_EMIT("vswp        d0, d2")
             __ASM_EMIT("vswp        d4, d6")
             __ASM_EMIT("vswp        d8, d10")
-            __ASM_EMIT("vswp        d12, d4")
+            __ASM_EMIT("vswp        d12, d14")
             __ASM_EMIT("vswp        d16, d18")
             __ASM_EMIT("vswp        d20, d22")
             __ASM_EMIT("vswp        d24, d26")
@@ -60,7 +60,7 @@ namespace neon_d32
             __ASM_EMIT("vswp        d0, d2")
             __ASM_EMIT("vswp        d4, d6")
             __ASM_EMIT("vswp        d8, d10")
-            __ASM_EMIT("vswp        d12, d4")
+            __ASM_EMIT("vswp        d12, d14")
             __ASM_EMIT("vst4.8      {q0-q1}, [%[dst]]!")
             __ASM_EMIT("vst4.8      {q2-q3}, [%[dst]]!")
             __ASM_EMIT("vst4.8      {q4-q5}, [%[dst]]!")
@@ -86,7 +86,7 @@ namespace neon_d32
             __ASM_EMIT("vld4.8      {q0-q1}, [%[src]]!") // d0 = R, d1 = G, d2 = B, d3 = A
             __ASM_EMIT("vswp        d0, d2")
             __ASM_EMIT("vst4.8      {q0-q1}, [%[dst]]!")
-            __ASM_EMIT("sub         %[count], $16")
+            __ASM_EMIT("sub         %[count], $8")
 
             // 4x blocks
             __ASM_EMIT("8:")
@@ -94,7 +94,7 @@ namespace neon_d32
             __ASM_EMIT("blt         10f")
             __ASM_EMIT("vld2.8      {q0}, [%[src]]!") // d0 = RB, d1 = GA
             __ASM_EMIT("vshl.i16    d2, d0, $8")
-            __ASM_EMIT("vshr.i16    d0, d0, $8")
+            __ASM_EMIT("vshr.u16    d0, d0, $8")
             __ASM_EMIT("vorr        d0, d2")
             __ASM_EMIT("vst2.8      {q0}, [%[dst]]!")
             __ASM_EMIT("sub         %[count], $4")
@@ -104,21 +104,20 @@ namespace neon_d32
             __ASM_EMIT("adds        %[count], $3")
             __ASM_EMIT("blt         12f")
             __ASM_EMIT("mov         %[mask], $0xff")
-            __ASM_EMIT("orr         %[mask], $0xff, lsl $16")           // mask = ff 00 ff 00
+            __ASM_EMIT("orr         %[mask], $0xff0000")           // mask = ff 00 ff 00
             __ASM_EMIT("11:")
-            __ASM_EMIT("ldr         %[t1], [%[src]]!")                  // t1 = R G B A
+            __ASM_EMIT("ldr         %[t1], [%[src]], $4")               // t1 = R G B A
             __ASM_EMIT("and         %[t2], %[t1], %[mask]")             // t2 = R 0 B 0
             __ASM_EMIT("and         %[t1], %[t1], %[mask], lsl $8")     // t1 = 0 G 0 A
-            __ASM_EMIT("orr         %[t1], %[t1], %[t2], lsl $16")      // t1 = 0 G R A
-            __ASM_EMIT("orr         %[t1], %[t1], %[t2], lsr $16")      // t1 = B G R A
-            __ASM_EMIT("str         %[t1], [%[dst]]!")
-            __ASM_EMIT("sub         %[count], $1")
+            __ASM_EMIT("orr         %[t1], %[t1], %[t2], ror $16")      // t1 = B G R A
+            __ASM_EMIT("str         %[t1], [%[dst]], $4")
+            __ASM_EMIT("subs        %[count], $1")
             __ASM_EMIT("bge         11b")
 
             __ASM_EMIT("12:")
-            : "+r" (src), "+r" (dst), "+r" (count)
-              (t1) "=&r" (t1), (t2) "=&r" (t2), (mask) "=&r" (mask)
-            : "m" (MASK)
+            : [src] "+r" (src), [dst] "+r" (dst), [count] "+r" (count),
+              [t1] "=&r" (t1), [t2] "=&r" (t2), [mask] "=&r" (mask)
+            :
             : "cc", "memory",
               "q0", "q1", "q2", "q3" , "q4", "q5", "q6", "q7",
               "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15"
