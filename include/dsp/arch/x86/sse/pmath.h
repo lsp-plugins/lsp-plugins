f0ef742d2d7843f79b964b408e35e17af95612a4
SadKo <lsp.plugin@gmail.com>
2018-09-21 23:39:22 +0300

    Rewritten all sse::scale methods

--- include/dsp/arch/x86/sse/pmath.h
+++ include/dsp/arch/x86/sse/pmath.h
@@ -483,110 +483,70 @@ namespace sse
     #undef SCALE_OP3_CORE
 
 #define SCALE_OP4_CORE(OP, DST, SRC1, SRC2)  \
-    __ASM_EMIT("xorps       %[off], %[off]") \
+    __ASM_EMIT("xor         %[off], %[off]") \
+    __ASM_EMIT("sub         $12, %[count]") \
     __ASM_EMIT("shufps      $0x00, %%xmm0, %%xmm0") \
-    __ASM_EMIT("sub         $8, %[count]") \
-    __ASM_EMIT("movaps      %%xmm0, %%xmm1") \
     __ASM_EMIT("jb          2f")    \
     \
-    /* 8x blocks */ \
+    /* 12x blocks */ \
+    __ASM_EMIT("movaps      %%xmm0, %%xmm1") \
     __ASM_EMIT("1:") \
-    __ASM_EMIT("movups      0x00(%[" SRC2 "]), %%xmm2") \
-    __ASM_EMIT("movups      0x10(%[src2]), %%xmm3") \
-    __ASM_EMIT("movups      0x00(%[src1]), %%xmm4") \
-    __ASM_EMIT("movups      0x10(%[src1]), %%xmm5") \
-    __ASM_EMIT("mulps       %%xmm0, %%xmm2") \
-    __ASM_EMIT("mulps       %%xmm1, %%xmm3") \
-    __ASM_EMIT(OP "ps       %%xmm2, %%xmm4") \
-    __ASM_EMIT(OP "ps       %%xmm3, %%xmm5") \
-    __ASM_EMIT("movaps      %%xmm4, 0x00(%[dst])") \
-    __ASM_EMIT("movaps      %%xmm5, 0x10(%[dst])") \
-    __ASM_EMIT("add         $0x20, %[src2]") \
-    __ASM_EMIT("add         $0x20, %[src1]") \
-    __ASM_EMIT("add         $0x20, %[dst]") \
-    __ASM_EMIT("sub         $0x08, %[count]") \
+    __ASM_EMIT("movups      0x00(%[" SRC1 "], %[off]), %%xmm2") \
+    __ASM_EMIT("movups      0x10(%[" SRC1 "], %[off]), %%xmm3") \
+    __ASM_EMIT("movups      0x20(%[" SRC1 "], %[off]), %%xmm4") \
+    __ASM_EMIT("movups      0x00(%[" SRC2 "], %[off]), %%xmm5") \
+    __ASM_EMIT("movups      0x10(%[" SRC2 "], %[off]), %%xmm6") \
+    __ASM_EMIT("movups      0x20(%[" SRC2 "], %[off]), %%xmm7") \
+    __ASM_EMIT("mulps       %%xmm0, %%xmm5") \
+    __ASM_EMIT("mulps       %%xmm1, %%xmm6") \
+    __ASM_EMIT("mulps       %%xmm0, %%xmm7") \
+    __ASM_EMIT(OP "ps       %%xmm5, %%xmm2") \
+    __ASM_EMIT(OP "ps       %%xmm6, %%xmm3") \
+    __ASM_EMIT(OP "ps       %%xmm7, %%xmm4") \
+    __ASM_EMIT("movups      %%xmm2, 0x00(%[" DST "], %[off])") \
+    __ASM_EMIT("movups      %%xmm3, 0x10(%[" DST "], %[off])") \
+    __ASM_EMIT("movups      %%xmm4, 0x20(%[" DST "], %[off])") \
+    __ASM_EMIT("add         $0x30, %[off]") \
+    __ASM_EMIT("sub         $12, %[count]") \
     __ASM_EMIT("jae         1b") \
-    \
-    /* 4x block */ \
-    __ASM_EMIT("4:") \
-    __ASM_EMIT("add         $0x08, %[count]") \
-    __ASM_EMIT("test        $0x04, %[count]") \
-    __ASM_EMIT("jz          5f") \
-    __ASM_EMIT(MV_SRC2 "    0x00(%[src2]), %%xmm2") \
-    __ASM_EMIT(MV_SRC1 "    0x00(%[src1]), %%xmm4") \
-    __ASM_EMIT("mulps       %%xmm0, %%xmm2") \
-    __ASM_EMIT(OP "ps       %%xmm2, %%xmm4") \
-    __ASM_EMIT("movaps      %%xmm4, 0x00(%[dst])") \
-    __ASM_EMIT("add         $0x10, %[src2]") \
-    __ASM_EMIT("add         $0x10, %[src1]") \
-    __ASM_EMIT("add         $0x10, %[dst]") \
+    /* 4x blocks */ \
+    __ASM_EMIT("2:") \
+    __ASM_EMIT("add         $8, %[count]") \
+    __ASM_EMIT("jl          4f") \
+    __ASM_EMIT("3:") \
+    __ASM_EMIT("movups      0x00(%[" SRC1 "], %[off]), %%xmm2") \
+    __ASM_EMIT("movups      0x00(%[" SRC2 "], %[off]), %%xmm5") \
+    __ASM_EMIT("mulps       %%xmm0, %%xmm5") \
+    __ASM_EMIT(OP "ps       %%xmm5, %%xmm2") \
+    __ASM_EMIT("movups      %%xmm2, 0x00(%[" DST "], %[off])") \
+    __ASM_EMIT("add         $0x10, %[off]") \
+    __ASM_EMIT("sub         $4, %[count]") \
+    __ASM_EMIT("jge         3b") \
     /* 1x blocks */ \
+    __ASM_EMIT("4:") \
+    __ASM_EMIT("add         $3, %[count]")    \
+    __ASM_EMIT("jl          6f")    \
     __ASM_EMIT("5:") \
-    __ASM_EMIT("and         $0x03, %[count]")    \
-    __ASM_EMIT("jz          2000f")    \
-    __ASM_EMIT("6:") \
-    __ASM_EMIT("movss       0x00(%[src2]), %%xmm2") \
-    __ASM_EMIT("movss       0x00(%[src1]), %%xmm4") \
-    __ASM_EMIT("mulss       %%xmm0, %%xmm2") \
-    __ASM_EMIT(OP "ss       %%xmm2, %%xmm4") \
-    __ASM_EMIT("movss       %%xmm4, 0x00(%[dst])") \
-    __ASM_EMIT("add         $0x4, %[src2]") \
-    __ASM_EMIT("add         $0x4, %[src1]") \
-    __ASM_EMIT("add         $0x4, %[dst]") \
+    __ASM_EMIT("movss       0x00(%[" SRC1 "], %[off]), %%xmm2") \
+    __ASM_EMIT("movss       0x00(%[" SRC2 "], %[off]), %%xmm5") \
+    __ASM_EMIT("mulss       %%xmm0, %%xmm5") \
+    __ASM_EMIT(OP "ss       %%xmm5, %%xmm2") \
+    __ASM_EMIT("movss       %%xmm2, 0x00(%[" DST "], %[off])") \
+    __ASM_EMIT("add         $0x4, %[off]") \
     __ASM_EMIT("dec         %[count]") \
-    __ASM_EMIT("jnz         6b")
-
-#define SCALE_OP4_ALIGN(OP)  \
-    __ASM_EMIT("1:") \
-    __ASM_EMIT("test        $0x0f, %[dst]") \
-    __ASM_EMIT("jz          2f") \
-    __ASM_EMIT("movss       0x00(%[src2]), %%xmm2") \
-    __ASM_EMIT("movss       0x00(%[src1]), %%xmm4") \
-    __ASM_EMIT("mulss       %%xmm0, %%xmm2") \
-    __ASM_EMIT(OP "ss       %%xmm2, %%xmm4") \
-    __ASM_EMIT("movss       %%xmm4, 0x00(%[dst])") \
-    __ASM_EMIT("add         $0x4, %[src2]") \
-    __ASM_EMIT("add         $0x4, %[src1]") \
-    __ASM_EMIT("add         $0x4, %[dst]") \
-    __ASM_EMIT("dec         %[count]") \
-    __ASM_EMIT("jnz         1b") \
-    __ASM_EMIT("jmp         2000f")
+    __ASM_EMIT("jge         5b") \
+    \
+    __ASM_EMIT("6:")
 
     void scale_add4(float *dst, const float *src1, const float *src2, float k, size_t count)
     {
+        size_t off;
         ARCH_X86_ASM
         (
-            __ASM_EMIT("test        %[count], %[count]")
-            __ASM_EMIT("jz          2000f")
-            __ASM_EMIT("shufps      $0x00, %%xmm0, %%xmm0")
-
-            // Align destination
-            SCALE_OP4_ALIGN("add")
-
-            // Block operation
-            __ASM_EMIT("2:")
-            __ASM_EMIT("movaps      %%xmm0, %%xmm1")
-            __ASM_EMIT("test        $0x0f, %[src1]")
-            __ASM_EMIT("jnz         1010f")
-                __ASM_EMIT("test        $0x0f, %[src2]")
-                __ASM_EMIT("jnz         1001f")
-                    SCALE_OP4_CORE("add", "movaps", "movaps")
-                    __ASM_EMIT("jmp         2000f")
-                __ASM_EMIT("1001:")
-                    SCALE_OP4_CORE("add", "movaps", "movups")
-                    __ASM_EMIT("jmp         2000f")
-
-            __ASM_EMIT("1010:")
-                __ASM_EMIT("test        $0x0f, %[src2]")
-                __ASM_EMIT("jnz         1011f")
-                    SCALE_OP4_CORE("add", "movups", "movaps")
-                    __ASM_EMIT("jmp         2000f")
-                __ASM_EMIT("1011:")
-                    SCALE_OP4_CORE("add", "movups", "movups")
-
-            __ASM_EMIT("2000:")
-
-            : [dst] "+r"(dst), [src1] "+r"(src1), [src2] "+r"(src2), [count] "+r" (count), [k] "+Yz"(k) :
+            SCALE_OP4_CORE("add", "dst", "src1", "src2")
+            : [count] "+r" (count), [off] "=&r" (off),
+              [k] "+Yz"(k)
+            : [dst] "r"(dst), [src1] "r"(src1), [src2] "r"(src2)
             : "cc", "memory",
               "%xmm1", "%xmm2", "%xmm3",
               "%xmm4", "%xmm5", "%xmm6", "%xmm7"
@@ -595,39 +555,13 @@ namespace sse
 
     void scale_sub4(float *dst, const float *src1, const float *src2, float k, size_t count)
     {
+        size_t off;
         ARCH_X86_ASM
         (
-            __ASM_EMIT("test        %[count], %[count]")
-            __ASM_EMIT("jz          2000f")
-            __ASM_EMIT("shufps      $0x00, %%xmm0, %%xmm0")
-
-            // Align destination
-            SCALE_OP4_ALIGN("sub")
-
-            // Block operation
-            __ASM_EMIT("2:")
-            __ASM_EMIT("movaps      %%xmm0, %%xmm1")
-            __ASM_EMIT("test        $0x0f, %[src1]")
-            __ASM_EMIT("jnz         1010f")
-                __ASM_EMIT("test        $0x0f, %[src2]")
-                __ASM_EMIT("jnz         1001f")
-                    SCALE_OP4_CORE("sub", "movaps", "movaps")
-                    __ASM_EMIT("jmp         2000f")
-                __ASM_EMIT("1001:")
-                    SCALE_OP4_CORE("sub", "movaps", "movups")
-                    __ASM_EMIT("jmp         2000f")
-
-            __ASM_EMIT("1010:")
-                __ASM_EMIT("test        $0x0f, %[src2]")
-                __ASM_EMIT("jnz         1011f")
-                    SCALE_OP4_CORE("sub", "movups", "movaps")
-                    __ASM_EMIT("jmp         2000f")
-                __ASM_EMIT("1011:")
-                    SCALE_OP4_CORE("sub", "movups", "movups")
-
-            __ASM_EMIT("2000:")
-
-            : [dst] "+r"(dst), [src1] "+r"(src1), [src2] "+r"(src2), [count] "+r" (count), [k] "+Yz"(k) :
+            SCALE_OP4_CORE("sub", "dst", "src1", "src2")
+            : [count] "+r" (count), [off] "=&r" (off),
+              [k] "+Yz"(k)
+            : [dst] "r"(dst), [src1] "r"(src1), [src2] "r"(src2)
             : "cc", "memory",
               "%xmm1", "%xmm2", "%xmm3",
               "%xmm4", "%xmm5", "%xmm6", "%xmm7"
@@ -636,39 +570,13 @@ namespace sse
 
     void scale_mul4(float *dst, const float *src1, const float *src2, float k, size_t count)
     {
+        size_t off;
         ARCH_X86_ASM
         (
-            __ASM_EMIT("test        %[count], %[count]")
-            __ASM_EMIT("jz          2000f")
-            __ASM_EMIT("shufps      $0x00, %%xmm0, %%xmm0")
-
-            // Align destination
-            SCALE_OP4_ALIGN("mul")
-
-            // Block operation
-            __ASM_EMIT("2:")
-            __ASM_EMIT("movaps      %%xmm0, %%xmm1")
-            __ASM_EMIT("test        $0x0f, %[src1]")
-            __ASM_EMIT("jnz         1010f")
-                __ASM_EMIT("test        $0x0f, %[src2]")
-                __ASM_EMIT("jnz         1001f")
-                    SCALE_OP4_CORE("mul", "movaps", "movaps")
-                    __ASM_EMIT("jmp         2000f")
-                __ASM_EMIT("1001:")
-                    SCALE_OP4_CORE("mul", "movaps", "movups")
-                    __ASM_EMIT("jmp         2000f")
-
-            __ASM_EMIT("1010:")
-                __ASM_EMIT("test        $0x0f, %[src2]")
-                __ASM_EMIT("jnz         1011f")
-                    SCALE_OP4_CORE("mul", "movups", "movaps")
-                    __ASM_EMIT("jmp         2000f")
-                __ASM_EMIT("1011:")
-                    SCALE_OP4_CORE("mul", "movups", "movups")
-
-            __ASM_EMIT("2000:")
-
-            : [dst] "+r"(dst), [src1] "+r"(src1), [src2] "+r"(src2), [count] "+r" (count), [k] "+Yz"(k) :
+            SCALE_OP4_CORE("mul", "dst", "src1", "src2")
+            : [count] "+r" (count), [off] "=&r" (off),
+              [k] "+Yz"(k)
+            : [dst] "r"(dst), [src1] "r"(src1), [src2] "r"(src2)
             : "cc", "memory",
               "%xmm1", "%xmm2", "%xmm3",
               "%xmm4", "%xmm5", "%xmm6", "%xmm7"
@@ -677,39 +585,13 @@ namespace sse
 
     void scale_div4(float *dst, const float *src1, const float *src2, float k, size_t count)
     {
+        size_t off;
         ARCH_X86_ASM
         (
-            __ASM_EMIT("test        %[count], %[count]")
-            __ASM_EMIT("jz          2000f")
-            __ASM_EMIT("shufps      $0x00, %%xmm0, %%xmm0")
-
-            // Align destination
-            SCALE_OP4_ALIGN("div")
-
-            // Block operation
-            __ASM_EMIT("2:")
-            __ASM_EMIT("movaps      %%xmm0, %%xmm1")
-            __ASM_EMIT("test        $0x0f, %[src1]")
-            __ASM_EMIT("jnz         1010f")
-                __ASM_EMIT("test        $0x0f, %[src2]")
-                __ASM_EMIT("jnz         1001f")
-                    SCALE_OP4_CORE("div", "movaps", "movaps")
-                    __ASM_EMIT("jmp         2000f")
-                __ASM_EMIT("1001:")
-                    SCALE_OP4_CORE("div", "movaps", "movups")
-                    __ASM_EMIT("jmp         2000f")
-
-            __ASM_EMIT("1010:")
-                __ASM_EMIT("test        $0x0f, %[src2]")
-                __ASM_EMIT("jnz         1011f")
-                    SCALE_OP4_CORE("div", "movups", "movaps")
-                    __ASM_EMIT("jmp         2000f")
-                __ASM_EMIT("1011:")
-                    SCALE_OP4_CORE("div", "movups", "movups")
-
-            __ASM_EMIT("2000:")
-
-            : [dst] "+r"(dst), [src1] "+r"(src1), [src2] "+r"(src2), [count] "+r" (count), [k] "+Yz"(k) :
+            SCALE_OP4_CORE("div", "dst", "src1", "src2")
+            : [count] "+r" (count), [off] "=&r" (off),
+              [k] "+Yz"(k)
+            : [dst] "r"(dst), [src1] "r"(src1), [src2] "r"(src2)
             : "cc", "memory",
               "%xmm1", "%xmm2", "%xmm3",
               "%xmm4", "%xmm5", "%xmm6", "%xmm7"
@@ -717,7 +599,6 @@ namespace sse
     }
 }
 
-#undef SCALE_OP4_ALIGN
 #undef SCALE_OP4_CORE
 
 #endif /* DSP_ARCH_X86_SSE_PMATH_H_ */
--- src/test/utest/dsp/pmath/scale.cpp
+++ src/test/utest/dsp/pmath/scale.cpp
@@ -22,6 +22,11 @@ namespace native
     void    scale_sub3(float *dst, const float *src, float k, size_t count);
     void    scale_mul3(float *dst, const float *src, float k, size_t count);
     void    scale_div3(float *dst, const float *src, float k, size_t count);
+
+    void    scale_add4(float *dst, const float *src1, const float *src2, float k, size_t count);
+    void    scale_sub4(float *dst, const float *src1, const float *src2, float k, size_t count);
+    void    scale_mul4(float *dst, const float *src1, const float *src2, float k, size_t count);
+    void    scale_div4(float *dst, const float *src1, const float *src2, float k, size_t count);
 }
 
 IF_ARCH_X86(
@@ -34,11 +39,17 @@ IF_ARCH_X86(
         void    scale_sub3(float *dst, const float *src, float k, size_t count);
         void    scale_mul3(float *dst, const float *src, float k, size_t count);
         void    scale_div3(float *dst, const float *src, float k, size_t count);
+
+        void    scale_add4(float *dst, const float *src1, const float *src2, float k, size_t count);
+        void    scale_sub4(float *dst, const float *src1, const float *src2, float k, size_t count);
+        void    scale_mul4(float *dst, const float *src1, const float *src2, float k, size_t count);
+        void    scale_div4(float *dst, const float *src1, const float *src2, float k, size_t count);
     }
 )
 
 typedef void (* scale2_t)(float *dst, float k, size_t count);
 typedef void (* scale3_t)(float *dst, const float *src, float k, size_t count);
+typedef void (* scale4_t)(float *dst, const float *src1, const float *src2, float k, size_t count);
 
 //-----------------------------------------------------------------------------
 // Unit test for complex multiplication
@@ -110,7 +121,48 @@ UTEST_BEGIN("dsp.pmath", scale)
                 // Compare buffers
                 if (!dst1.equals_absolute(dst2, 1e-5))
                 {
-                    src.dump("src1");
+                    src.dump("src ");
+                    dst1.dump("dst1");
+                    dst2.dump("dst2");
+                    UTEST_FAIL_MSG("Output of functions for test '%s' differs", label);
+                }
+            }
+        }
+    }
+
+    void call(const char *label, size_t align, scale4_t func1, scale4_t func2)
+    {
+        if (!UTEST_SUPPORTED(func1))
+            return;
+        if (!UTEST_SUPPORTED(func2))
+            return;
+
+        UTEST_FOREACH(count, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                32, 64, 65, 100, 999)
+        {
+            for (size_t mask=0; mask <= 0x07; ++mask)
+            {
+                printf("Testing %s on input buffer of %d numbers, mask=0x%x...\n", label, int(count), int(mask));
+
+                FloatBuffer src1(count, align, mask & 0x01);
+                FloatBuffer src2(count, align, mask & 0x02);
+                FloatBuffer dst1(count, align, mask & 0x04);
+                FloatBuffer dst2(dst1);
+
+                // Call functions
+                func1(dst1, src1, src2, 0.5f, count);
+                func2(dst2, src1, src2, 0.5f, count);
+
+                UTEST_ASSERT_MSG(src1.valid(), "Source buffer 1 corrupted");
+                UTEST_ASSERT_MSG(src2.valid(), "Source buffer 2 corrupted");
+                UTEST_ASSERT_MSG(dst1.valid(), "Destination buffer 1 corrupted");
+                UTEST_ASSERT_MSG(dst2.valid(), "Destination buffer 2 corrupted");
+
+                // Compare buffers
+                if (!dst1.equals_absolute(dst2, 1e-5))
+                {
+                    src1.dump("src1");
+                    src2.dump("src2");
                     dst1.dump("dst1");
                     dst2.dump("dst2");
                     UTEST_FAIL_MSG("Output of functions for test '%s' differs", label);
@@ -127,6 +179,10 @@ UTEST_BEGIN("dsp.pmath", scale)
         IF_ARCH_X86(call("sse:scale_sub3", 16, native::scale_sub3, sse::scale_sub3));
         IF_ARCH_X86(call("sse:scale_mul3", 16, native::scale_mul3, sse::scale_mul3));
         IF_ARCH_X86(call("sse:scale_div3", 16, native::scale_div3, sse::scale_div3));
+        IF_ARCH_X86(call("sse:scale_add4", 16, native::scale_add4, sse::scale_add4));
+        IF_ARCH_X86(call("sse:scale_sub4", 16, native::scale_sub4, sse::scale_sub4));
+        IF_ARCH_X86(call("sse:scale_mul4", 16, native::scale_mul4, sse::scale_mul4));
+        IF_ARCH_X86(call("sse:scale_div4", 16, native::scale_div4, sse::scale_div4));
     }
 UTEST_END
 
