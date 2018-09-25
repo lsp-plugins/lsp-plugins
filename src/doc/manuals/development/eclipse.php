c85094b81ac245daf0d87366d8d3fffc635934dd
SadKo <lsp.plugin@gmail.com>
2018-09-26 00:18:17 +0300

    Updated documentation and styles

--- src/doc/manuals/development/eclipse.php
+++ src/doc/manuals/development/eclipse.php
@@ -21,18 +21,18 @@ git checkout -b &lt;your-branch&gt; origin/devel
 </pre>
 
 <p>Now we can switch to C++ development perspective by selecting
-<b>Window</b> -> <b>Perspective</b> -> <b>Open Perspective</b> -> <b>Other...</b></p>
+<b>Window</b> &rarr; <b>Perspective</b> &rarr; <b>Open Perspective</b> &rarr; <b>Other...</b></p>
 <div class="images">
 	<img src="<?= $IMG ?>eclipse-cpp-perspective.png">
 </div>
 
-<p>After switching to C/C++ perspective, select <b>File</b> -> <b>New</b> -> <b>C/C++ Project</b></p>
+<p>After switching to C/C++ perspective, select <b>File</b> &rarr; <b>New</b> &rarr; <b>C/C++ Project</b></p>
 <p>In the opened dialog, select <b>C++ Managed Build</b> template, then click <b>Next</b></p>
 <div class="images">
 	<img src="<?= $IMG ?>eclipse-cpp-template.png">
 </div>
 
-<p>Enter the folder name same to the folder of the GIT repository, select <b>Executable</b> -> <b>Empty Project</b>,
+<p>Enter the folder name same to the folder of the GIT repository, select <b>Executable</b> &rarr; <b>Empty Project</b>,
 <b>Linux GCC</b>, then click <b>Finish</b></p>
 <div class="images">
 	<img src="<?= $IMG ?>eclipse-new-project.png">
@@ -43,20 +43,20 @@ git checkout -b &lt;your-branch&gt; origin/devel
 	<img src="<?= $IMG ?>eclipse-project-explorer.png">
 </div>
 
-<p>We just need to configure project build. For this case, select <b>Project</b> -> <b>Properties</b> menu,
+<p>We just need to configure project build. For this case, select <b>Project</b> &rarr; <b>Properties</b> menu,
 the project configuration dialog should appear:</p> 
 <div class="images">
 	<img src="<?= $IMG ?>eclipse-project-properties.png">
 </div>
 
-<p>Go to <b>C/C++ Build</b> -> <b>Settings</b> and, first of all, select <b>Configuration</b> combo
+<p>Go to <b>C/C++ Build</b> &rarr; <b>Settings</b> and, first of all, select <b>Configuration</b> combo
 to <b>[ All Configurations ]</b>, this will apply all modified settings to all build targets.</p>
-<p>Select <b>GCC C++ Compiler</b> -> <b>Dialect</b> section and select the <b>ISO C++98</b> as a <b>Language Standard</b>:</p>
+<p>Select <b>GCC C++ Compiler</b> &rarr; <b>Dialect</b> section and select the <b>ISO C++98</b> as a <b>Language Standard</b>:</p>
 <div class="images">
 	<img src="<?= $IMG ?>eclipse-project-lang-std.png">
 </div>
 
-<p>Select <b>GCC C++ Compiler</b> -> <b>Preprocessor</b> section and add following macros to <b>Defined Symbols</b>:</p>
+<p>Select <b>GCC C++ Compiler</b> &rarr; <b>Preprocessor</b> section and add following macros to <b>Defined Symbols</b>:</p>
 <ul>
 	<li><b>LSP_TRACE</b> - this macro will enable output of trace information to the console while running code in debug envirionment.</li>
 	<li><b>LSP_IDE_DEBUG</b> - makes invisible set of main() functions that are defined in source code of different tools.</li>
@@ -67,7 +67,7 @@ to <b>[ All Configurations ]</b>, this will apply all modified settings to all b
 	<img src="<?= $IMG ?>eclipse-preprocessor-setup.png">
 </div>
 
-<p>Now we need to define additional location of header files. Select <b>GCC C++ Compiler</b> -> <b>Includes</b>
+<p>Now we need to define additional location of header files. Select <b>GCC C++ Compiler</b> &rarr; <b>Includes</b>
 and add to the <b>Include paths</b> the following line:</p>
 <pre>
 ${workspace_loc:/${ProjName}/include}
@@ -77,7 +77,7 @@ ${workspace_loc:/${ProjName}/include}
 	<img src="<?= $IMG ?>eclipse-project-includes.png">
 </div>
 
-<p>Now we need to enable extra headers for additional packages, for this purpose visit <b>GCC C++ Compiler</b> -> <b>Miscellaneous</b>
+<p>Now we need to enable extra headers for additional packages, for this purpose visit <b>GCC C++ Compiler</b> &rarr; <b>Miscellaneous</b>
 and enter into the <b>Other flags</b> field the following string:</p>
 <pre>
 -c -fmessage-length=0 -m64 -msse -msse2 `pkg-config --cflags jack expat x11 cairo lv2 sndfile gl`
@@ -87,22 +87,22 @@ and enter into the <b>Other flags</b> field the following string:</p>
 	<img src="<?= $IMG ?>eclipse-project-misc.png">
 </div>
 
-<p>Select <b>GCC C++ Linker</b> -> <b>General</b> settings and enable the <b>Support for pthread (-pthread)</b> option:<p>
+<p>Select <b>GCC C++ Linker</b> &rarr; <b>General</b> settings and enable the <b>Support for pthread (-pthread)</b> option:<p>
 <div class="images">
 	<img src="<?= $IMG ?>eclipse-project-linker-general.png">
 </div>
 
-<p>Select <b>GCC C++ Linker</b> -> <b>General</b> settings and enable the <b>Support for pthread (-pthread)</b> option:<p>
+<p>Select <b>GCC C++ Linker</b> &rarr; <b>General</b> settings and enable the <b>Support for pthread (-pthread)</b> option:<p>
 <div class="images">
 	<img src="<?= $IMG ?>eclipse-project-linker-general.png">
 </div>
 
-<p>Select <b>GCC C++ Linker</b> -> <b>General</b> settings and enable the <b>Support for pthread (-pthread)</b> option:<p>
+<p>Select <b>GCC C++ Linker</b> &rarr; <b>General</b> settings and enable the <b>Support for pthread (-pthread)</b> option:<p>
 <div class="images">
 	<img src="<?= $IMG ?>eclipse-project-linker-general.png">
 </div>
 
-<p>Select <b>GCC C++ Linker</b> -> <b>Miscellaneous</b> settings and enter the following string to <b>Linker flags</b> field
+<p>Select <b>GCC C++ Linker</b> &rarr; <b>Miscellaneous</b> settings and enter the following string to <b>Linker flags</b> field
 to enable linkage with external libraries:<p>
 <pre>
 `pkg-config --libs jack expat x11 cairo lv2 sndfile gl`
@@ -113,7 +113,7 @@ to enable linkage with external libraries:<p>
 </div>
 
 <p>To make resources generated by makefile invisible to IDE, we need to add <b>.build</b> path as excluded from source tree.
-For this purpose, visit <b>Resource</b> -> <b>Resource Filters</b>, click <b>Add Filter</b></p>
+For this purpose, visit <b>Resource</b> &rarr; <b>Resource Filters</b>, click <b>Add Filter</b></p>
 <p>Set <b>Filter type</b> to <b>Exclude all</b>, <b>Applies to</b> - <b>Folders</b>, <b>All children (recursive)</b>,
 <b>Name</b> <b>Matches</b> <b>.build</b>. The result should look like:</p>
 <div class="images">
@@ -121,23 +121,23 @@ For this purpose, visit <b>Resource</b> -> <b>Resource Filters</b>, click <b>Add
 </div>
 
 <p>To optimize build, we can enable more than one thread for build. For this purpose, select
-<b>C/C++ Build</b> -> <b>Behavior</b> and click <b>Enable Parallel Build</b>, <b>Use optimal jobs</b>:</p>
+<b>C/C++ Build</b> &rarr; <b>Behavior</b> and click <b>Enable Parallel Build</b>, <b>Use optimal jobs</b>:</p>
 <div class="images">
 	<img src="<?= $IMG ?>eclipse-project-build.png">
 </div>
 
-<p>Now we can click <b>Apply and Close</b> button and launch build by issuing <b>Project</b> -> <b>Build</b> menu item.</p>
+<p>Now we can click <b>Apply and Close</b> button and launch build by issuing <b>Project</b> &rarr; <b>Build</b> menu item.</p>
 
 <h2>Debugging Project</h2>
 
-<p>To start debugging, simply issue <b>Run</b> -> <b>Debug as</b> -> <b>Local C/C++ Application</b>, Eclipse IDE will
+<p>To start debugging, simply issue <b>Run</b> &rarr; <b>Debug as</b> &rarr; <b>Local C/C++ Application</b>, Eclipse IDE will
 automatically create configuration for launching build that can be reused in future.</p>
 <p>The main() function launches testing subsystem, so in console output we will get the following string:</p>
 <pre>
 USAGE: {utest|ptest|mtest} [args...] [test name...]
 </pre>
 
-<p>Now we can edit configuration by selecting <b>Run</b> -> <b>Debug Configurations...</b> and pass additional
+<p>Now we can edit configuration by selecting <b>Run</b> &rarr; <b>Debug Configurations...</b> and pass additional
 parameters to unit testing subsystem. Select the generated <b>lsp-plugins</b> debug profile, <b>Arguments</b> tab
 and enter testing subsystem command-line arguments. For example, the following:</p>
 <pre>
