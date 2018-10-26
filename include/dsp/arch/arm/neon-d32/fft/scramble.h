88521d2d4a16a31a7a141e49bba814036584d5b1
sadko4u <sadko4u@gmail.com>
2018-10-26 12:47:06 +0300

    Implemented bit-reverse shuffle for direct FFT for ARM.

--- include/dsp/arch/arm/neon-d32/fft/scramble.h
+++ include/dsp/arch/arm/neon-d32/fft/scramble.h
@@ -19,7 +19,7 @@ namespace neon_d32
         if ((dst_re == src_re) || (dst_im == src_im))
         {
             IF_ARCH_ARM(
-                size_t count = 1 << rank
+                size_t count = 1 << rank;
                 size_t i, j, rrank = rank;
                 float *d_re, *d_im;
             );
@@ -29,7 +29,7 @@ namespace neon_d32
                 // Do bit-reverse shuffle
                 __ASM_EMIT("rsb         %[rrank], %[rrank], $32")           // rrank = 32 - rank
                 __ASM_EMIT("push        {%[dst_re], %[dst_im], %[src_re], %[src_im]}")
-                __ASM_EMIT("vmov        %[i], $1")                          // i = 1
+                __ASM_EMIT("mov         %[i], $1")                          // i = 1
 
                 __ASM_EMIT("1:")
                 __ASM_EMIT("rbit        %[j], %[i]")                        // j = reverse_bits(i)
@@ -44,8 +44,8 @@ namespace neon_d32
                 __ASM_EMIT("vldm        %[src_im], {s1}")                   // s1 = *src_im
                 __ASM_EMIT("vldm        %[d_re], {s2}")                     // s2 = *td_re
                 __ASM_EMIT("vldm        %[d_im], {s3}")                     // s3 = *td_im
-                __ASM_EMIT("vstm        %[src_re]!, {s2}")                  // *(src_re++) = s2
-                __ASM_EMIT("vstm        %[src_im]!, {s3}")                  // *(src_im++) = s3
+                __ASM_EMIT("vstm        %[src_re], {s2}")                   // *(src_re++) = s2
+                __ASM_EMIT("vstm        %[src_im], {s3}")                   // *(src_im++) = s3
                 __ASM_EMIT("vstm        %[d_re], {s0}")                     // *d_re = s0
                 __ASM_EMIT("vstm        %[d_im], {s1}")                     // *d_im = s1
                 __ASM_EMIT("2:")
@@ -109,54 +109,54 @@ namespace neon_d32
             );
 
             ARCH_ARM_ASM(
-                __ASM_EMIT("veor        %[i], %[i]")                        // i = 0
+                __ASM_EMIT("eor         %[i], %[i]")                         // i = 0
                 __ASM_EMIT("rsb         %[rrank], %[rrank], $32")           // rrank = 32 - rank
 
                 __ASM_EMIT("1:")
                 __ASM_EMIT("rbit        %[j], %[i]")                        // j = reverse_bits(i)
                 __ASM_EMIT("lsr         %[j], %[rrank]")                    // j = reverse_bits(i) >> rank
 
-                __ASM_EMIT("add         %[s_re], %[src_re], %[i], LSL $2")  // s_re = &src_re[i]
-                __ASM_EMIT("add         %[s_im], %[src_im], %[i], LSL $2")  // s_re = &src_im[i]
-                __ASM_EMIT("vldm        %[s_re], s0")                       // q0 = r0 ? ? ?
-                __ASM_EMIT("vldm        %[s_im], s8")                       // q2 = i0 ? ? ?
+                __ASM_EMIT("add         %[s_re], %[src_re], %[j], LSL $2")  // s_re = &src_re[i]
+                __ASM_EMIT("add         %[s_im], %[src_im], %[j], LSL $2")  // s_re = &src_im[i]
+                __ASM_EMIT("vldm        %[s_re], {s0}")                     // q0 = r0 ? ? ?
+                __ASM_EMIT("vldm        %[s_im], {s8}")                     // q2 = i0 ? ? ?
                 __ASM_EMIT("add         %[s_re], %[s_re], %[regs], LSL $2") // s_re = &src_re[i + regs]
                 __ASM_EMIT("add         %[s_im], %[s_im], %[regs], LSL $2") // s_re = &src_im[i + regs]
-                __ASM_EMIT("vldm        %[s_re], s2")                       // q0 = r0 ? r4 ?
-                __ASM_EMIT("vldm        %[s_im], s10")                      // q2 = i0 ? i4 ?
+                __ASM_EMIT("vldm        %[s_re], {s2}")                     // q0 = r0 ? r4 ?
+                __ASM_EMIT("vldm        %[s_im], {s10}")                    // q2 = i0 ? i4 ?
                 __ASM_EMIT("add         %[s_re], %[s_re], %[regs], LSL $2") // s_re = &src_re[i + regs*2]
                 __ASM_EMIT("add         %[s_im], %[s_im], %[regs], LSL $2") // s_re = &src_im[i + regs*2]
-                __ASM_EMIT("vldm        %[s_re], s1")                       // q0 = r0 r2 r4 ?
-                __ASM_EMIT("vldm        %[s_im], s9")                       // q2 = i0 i2 i4 ?
+                __ASM_EMIT("vldm        %[s_re], {s1}")                     // q0 = r0 r2 r4 ?
+                __ASM_EMIT("vldm        %[s_im], {s9}")                     // q2 = i0 i2 i4 ?
                 __ASM_EMIT("add         %[s_re], %[s_re], %[regs], LSL $2") // s_re = &src_re[i + regs*3]
                 __ASM_EMIT("add         %[s_im], %[s_im], %[regs], LSL $2") // s_re = &src_im[i + regs*3]
-                __ASM_EMIT("vldm        %[s_re], s3")                       // q0 = r0 r2 r4 r6
-                __ASM_EMIT("vldm        %[s_im], s11")                      // q2 = i0 i2 i4 i6
+                __ASM_EMIT("vldm        %[s_re], {s3}")                     // q0 = r0 r2 r4 r6
+                __ASM_EMIT("vldm        %[s_im], {s11}")                    // q2 = i0 i2 i4 i6
 
                 __ASM_EMIT("add         %[s_re], %[s_re], %[regs], LSL $2") // s_re = &src_re[i + regs*4]
                 __ASM_EMIT("add         %[s_im], %[s_im], %[regs], LSL $2") // s_re = &src_im[i + regs*4]
-                __ASM_EMIT("vldm        %[s_re], s4")                       // q1 = r1 ? ? ?
-                __ASM_EMIT("vldm        %[s_im], s12")                      // q3 = i1 ? ? ?
+                __ASM_EMIT("vldm        %[s_re], {s4}")                     // q1 = r1 ? ? ?
+                __ASM_EMIT("vldm        %[s_im], {s12}")                    // q3 = i1 ? ? ?
                 __ASM_EMIT("add         %[s_re], %[s_re], %[regs], LSL $2") // s_re = &src_re[i + regs*5]
                 __ASM_EMIT("add         %[s_im], %[s_im], %[regs], LSL $2") // s_re = &src_im[i + regs*5]
-                __ASM_EMIT("vldm        %[s_re], s6")                       // q1 = r1 ? r5 ?
-                __ASM_EMIT("vldm        %[s_im], s14")                      // q3 = i1 ? i5 ?
+                __ASM_EMIT("vldm        %[s_re], {s6}")                     // q1 = r1 ? r5 ?
+                __ASM_EMIT("vldm        %[s_im], {s14}")                    // q3 = i1 ? i5 ?
                 __ASM_EMIT("add         %[s_re], %[s_re], %[regs], LSL $2") // s_re = &src_re[i + regs*6]
                 __ASM_EMIT("add         %[s_im], %[s_im], %[regs], LSL $2") // s_re = &src_im[i + regs*6]
-                __ASM_EMIT("vldm        %[s_re], s5")                       // q1 = r1 r3 r5 ?
-                __ASM_EMIT("vldm        %[s_im], s13")                      // q3 = i1 i3 i5 ?
+                __ASM_EMIT("vldm        %[s_re], {s5}")                     // q1 = r1 r3 r5 ?
+                __ASM_EMIT("vldm        %[s_im], {s13}")                    // q3 = i1 i3 i5 ?
                 __ASM_EMIT("add         %[s_re], %[s_re], %[regs], LSL $2") // s_re = &src_re[i + regs*7]
                 __ASM_EMIT("add         %[s_im], %[s_im], %[regs], LSL $2") // s_re = &src_im[i + regs*7]
-                __ASM_EMIT("vldm        %[s_re], s7")                       // q1 = r1 r3 r5 r7
-                __ASM_EMIT("vldm        %[s_im], s15")                      // q3 = i1 i3 i5 i7
+                __ASM_EMIT("vldm        %[s_re], {s7}")                     // q1 = r1 r3 r5 r7
+                __ASM_EMIT("vldm        %[s_im], {s15}")                    // q3 = i1 i3 i5 i7
 
                 // q0 = r0 r2 r4 r6
                 // q1 = r1 r3 r5 r7
                 // q2 = i0 i2 i4 i6
                 // q3 = i1 i3 i5 i7
                 __ASM_EMIT("add         %[i], $1")
-                __ASM_EMIT("vst2.32     {q0-q1}, %[dst_re]!")
-                __ASM_EMIT("vst2.32     {q2-q3}, %[dst_im]!")
+                __ASM_EMIT("vst2.32     {q0-q1}, [%[dst_re]]!")
+                __ASM_EMIT("vst2.32     {q2-q3}, [%[dst_im]]!")
                 __ASM_EMIT("cmp         %[i], %[regs]")
                 __ASM_EMIT("blo         1b")
 
