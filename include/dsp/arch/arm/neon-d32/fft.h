1eb2a449030f697e7c9d7212b89b8d53de566975
sadko4u <sadko4u@gmail.com>
2018-10-30 16:18:18 +0300

    Added prototype of packed_reverse_fft for ARM

--- include/dsp/arch/arm/neon-d32/fft.h
+++ include/dsp/arch/arm/neon-d32/fft.h
@@ -184,6 +184,61 @@ namespace neon_d32
 
         packed_unscramble_direct(dst, rank);
     }
+
+    void packed_reverse_fft(float *dst, const float *src, size_t rank)
+    {
+        // Check bounds
+        if (rank <= 2)
+        {
+            if (rank == 2)
+            {
+                float s0_re     = src[0] + src[2];
+                float s1_re     = src[0] - src[2];
+                float s2_re     = src[4] + src[6];
+                float s3_re     = src[4] - src[6];
+
+                float s0_im     = src[1] + src[3];
+                float s1_im     = src[1] - src[3];
+                float s2_im     = src[5] + src[7];
+                float s3_im     = src[5] - src[7];
+
+                dst[0]          = (s0_re + s2_re)*0.25f;
+                dst[1]          = (s0_im + s2_im)*0.25f;
+                dst[2]          = (s1_re - s3_im)*0.25f;
+                dst[3]          = (s1_im + s3_re)*0.25f;
+
+                dst[4]          = (s0_re - s2_re)*0.25f;
+                dst[5]          = (s0_im - s2_im)*0.25f;
+                dst[6]          = (s1_re + s3_im)*0.25f;
+                dst[7]          = (s1_im - s3_re)*0.25f;
+            }
+            else if (rank == 1)
+            {
+                // s0' = s0 + s1
+                // s1' = s0 - s1
+                float s1_re     = src[2];
+                float s1_im     = src[3];
+                dst[2]          = src[0] - s1_re;
+                dst[3]          = src[1] - s1_im;
+                dst[0]          = src[0] + s1_re;
+                dst[1]          = src[1] + s1_im;
+            }
+            else
+            {
+                dst[0]          = src[0];
+                dst[1]          = src[1];
+            }
+            return;
+        }
+
+        packed_scramble_reverse(dst, src, rank);
+        packed_reverse_butterfly_rank3(dst, 1 << (rank-3));
+
+        for (size_t i=4; i <= rank; ++i)
+            packed_reverse_butterfly_rank4p(dst, i, 1 << (rank - i));
+
+        packed_unscramble_reverse(dst, rank);
+    }
 }
 
 #endif /* DSP_ARCH_ARM_NEON_D32_FFT_H_ */
