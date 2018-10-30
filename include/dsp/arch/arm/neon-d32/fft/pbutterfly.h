05ce4d678b71ec442df521f07e38f9ad96a0cb6c
sadko4u <sadko4u@gmail.com>
2018-10-30 12:15:26 +0300

    Code update

--- include/dsp/arch/arm/neon-d32/fft/pbutterfly.h
+++ include/dsp/arch/arm/neon-d32/fft/pbutterfly.h
@@ -10,7 +10,6 @@
 
 namespace neon_d32
 {
-#if 0
     #define PBUTTERFLY_RANK3(op1, op2) \
         __ASM_EMIT("vldm        %[XFFT_A], {q8-q11}")           /* q8   = wr1, q9 = wr2, q10 = wi1, q11 = wi2 */ \
         __ASM_EMIT("subs        %[blocks], $2") \
@@ -20,11 +19,7 @@ namespace neon_d32
         /* Calculate complex c = w * b */ \
         __ASM_EMIT("1:") \
         /* Calc cr and ci */ \
-        __ASM_EMIT("vldm        %[dst], {q0-q7}")               \
-        __ASM_EMIT("vuzp.32     q0, q1")                        /* q0   = ar1, q1 = ai1 */ \
-        __ASM_EMIT("vuzp.32     q2, q3")                        /* q2   = br1, q3 = bi1 */ \
-        __ASM_EMIT("vuzp.32     q4, q5")                        /* q4   = ar2, q5 = ai2 */ \
-        __ASM_EMIT("vuzp.32     q6, q7")                        /* q6   = br2, q7 = bi2 */ \
+        __ASM_EMIT("vldm        %[dst], {q0-q7}")               /* q0   = ar1, q1 = ai1, q2 = br1, q3 = bi1, q4 = ar2, q5 = ai2, q6 = br2, q7 = bi2 */\
         __ASM_EMIT("vmul.f32    q12, q8, q2")                   /* q12  = wr1 * br1 */ \
         __ASM_EMIT("vmul.f32    q14, q8, q3")                   /* q14  = wr1 * bi1 */ \
         __ASM_EMIT("vmul.f32    q13, q9, q6")                   /* q13  = wr2 * br2 */ \
@@ -42,10 +37,6 @@ namespace neon_d32
         __ASM_EMIT("vadd.f32    q1, q1, q14")                   /* q1   = ai1 + ci1 */ \
         __ASM_EMIT("vadd.f32    q4, q4, q13")                   /* q4   = ar2 + cr2 */ \
         __ASM_EMIT("vadd.f32    q5, q5, q15")                   /* q5   = ai2 + ci2 */ \
-        __ASM_EMIT("vzip.32     q0, q1") \
-        __ASM_EMIT("vzip.32     q2, q3") \
-        __ASM_EMIT("vzip.32     q4, q5") \
-        __ASM_EMIT("vzip.32     q6, q7") \
         __ASM_EMIT("vstm        %[dst]!, {q0-q7}")               \
         __ASM_EMIT("subs        %[blocks], $2") \
         __ASM_EMIT("bge         1b") \
@@ -54,74 +45,18 @@ namespace neon_d32
         __ASM_EMIT("adds        %[blocks], $1") \
         __ASM_EMIT("blo         4f") \
         /* 4x butterflies */ \
-        __ASM_EMIT("vld2.32     {q0-q1}, [%[dst]]!")            /* q0   = ar1, q1 = ai1 */ \
-        __ASM_EMIT("vld2.32     {q2-q3}, [%[dst]]")             /* q2   = br1, q3 = bi1 */ \
+        __ASM_EMIT("vldm        %[dst], {q0-q3}")               /* q0   = ar1, q1 = ai1, q2 = br1, q3 = bi1 */ \
         __ASM_EMIT("vmul.f32    q12, q8, q2")                   /* q12  = wr1 * br1 */ \
         __ASM_EMIT("vmul.f32    q14, q8, q3")                   /* q14  = wr1 * bi1 */ \
         __ASM_EMIT(op1 "        q12, q10, q3")                  /* q12  = wr1 * br1 +- wi1 * bi1 = cr1 */ \
         __ASM_EMIT(op2 "        q14, q10, q2")                  /* q14  = wr1 * bi1 -+ wi1 * br1 = ci1 */ \
-        __ASM_EMIT("sub         %[dst], $0x20") \
         /* Apply butterfly */ \
         __ASM_EMIT("vsub.f32    q2, q0, q12")                   /* q2   = ar1 - cr1 */ \
         __ASM_EMIT("vsub.f32    q3, q1, q14")                   /* q3   = ai1 - ci1 */ \
         __ASM_EMIT("vadd.f32    q0, q0, q12")                   /* q0   = ar1 + cr1 */ \
         __ASM_EMIT("vadd.f32    q1, q1, q14")                   /* q1   = ai1 + ci1 */ \
-        __ASM_EMIT("vst2.32     {q0-q1}, [%[dst]]!") \
-        __ASM_EMIT("vst2.32     {q2-q3}, [%[dst]]") \
+        __ASM_EMIT("vstm        %[dst], {q0-q3}") \
         __ASM_EMIT("4:")
-#endif
-
-#define BUTTERFLY_RANK3(op1, op2) \
-    __ASM_EMIT("vldm        %[XFFT_A], {q8-q11}")           /* q8   = wr1, q9 = wr2, q10 = wi1, q11 = wi2 */ \
-    __ASM_EMIT("subs        %[blocks], $2") \
-    __ASM_EMIT("blo         2f") \
-    \
-    /* 8x butterflies */ \
-    /* Calculate complex c = w * b */ \
-    __ASM_EMIT("1:") \
-    __ASM_EMIT("vldm        %[dst_re], {q0-q7}")            /* q0   = ar1, q1 = br1, q2 = ar2, q3 = br2 */ \
-    __ASM_EMIT("vldm        %[dst_im], {q4-q7}")            /* q4 = ai1, q5 = bi1, q6 = ai2, q7 = bi2 */ \
-    /* Calc cr and ci */ \
-    __ASM_EMIT("vmul.f32    q12, q8, q1")                   /* q12  = wr1 * br1 */ \
-    __ASM_EMIT("vmul.f32    q13, q9, q3")                   /* q13  = wr2 * br2 */ \
-    __ASM_EMIT("vmul.f32    q14, q8, q5")                   /* q14  = wr1 * bi1 */ \
-    __ASM_EMIT("vmul.f32    q15, q9, q7")                   /* q15  = wr2 * bi2 */ \
-    __ASM_EMIT(op1 "        q12, q10, q5")                  /* q12  = wr1 * br1 +- wi1 * bi1 = cr1 */ \
-    __ASM_EMIT(op1 "        q13, q11, q7")                  /* q13  = wr2 * br2 +- wi2 * bi2 = cr2 */ \
-    __ASM_EMIT(op2 "        q14, q10, q1")                  /* q14  = wr1 * bi1 -+ wi1 * br1 = ci1 */ \
-    __ASM_EMIT(op2 "        q15, q11, q3")                  /* q15  = wr1 * bi1 -+ wi2 * br2 = ci2 */ \
-    /* Apply butterfly */ \
-    __ASM_EMIT("vsub.f32    q1, q0, q12")                   /* q1   = ar1 - cr1 */ \
-    __ASM_EMIT("vsub.f32    q3, q2, q13")                   /* q3   = ar2 - cr2 */ \
-    __ASM_EMIT("vsub.f32    q5, q4, q14")                   /* q5   = ai1 - ci1 */ \
-    __ASM_EMIT("vsub.f32    q7, q6, q15")                   /* q7   = ai2 - ci2 */ \
-    __ASM_EMIT("vadd.f32    q0, q0, q12")                   /* q0   = ar1 + cr1 */ \
-    __ASM_EMIT("vadd.f32    q2, q2, q13")                   /* q2   = ar2 + cr2 */ \
-    __ASM_EMIT("vadd.f32    q4, q4, q14")                   /* q4   = ai1 + ci1 */ \
-    __ASM_EMIT("vadd.f32    q6, q6, q15")                   /* q6   = ai2 + ci2 */ \
-    __ASM_EMIT("vstm        %[dst_re]!, {q0-q3}") \
-    __ASM_EMIT("vstm        %[dst_im]!, {q4-q7}") \
-    __ASM_EMIT("subs        %[blocks], $2") \
-    __ASM_EMIT("bge         1b") \
-    \
-    __ASM_EMIT("2:") \
-    __ASM_EMIT("adds        %[blocks], $1") \
-    __ASM_EMIT("blo         4f") \
-    /* 4x butterflies */ \
-    __ASM_EMIT("vldm        %[dst_re], {q0-q1}")            /* q0   = ar1, q1 = br1 */ \
-    __ASM_EMIT("vldm        %[dst_im], {q4-q5}")            /* q4   = ai1, q5 = bi1 */ \
-    __ASM_EMIT("vmul.f32    q12, q8, q1")                   /* q12  = wr1 * br1 */ \
-    __ASM_EMIT("vmul.f32    q14, q8, q5")                   /* q14  = wr1 * bi1 */ \
-    __ASM_EMIT(op1 "        q12, q10, q5")                  /* q12  = wr1 * br1 +- wi1 * bi1 = cr1 */ \
-    __ASM_EMIT(op2 "        q14, q10, q1")                  /* q14  = wr1 * bi1 -+ wi1 * br1 = ci1 */ \
-    /* Apply butterfly */ \
-    __ASM_EMIT("vsub.f32    q1, q0, q12")                   /* q1   = ar1 - cr1 */ \
-    __ASM_EMIT("vsub.f32    q5, q4, q14")                   /* q5   = ai1 - ci1 */ \
-    __ASM_EMIT("vadd.f32    q0, q0, q12")                   /* q0   = ar1 + cr1 */ \
-    __ASM_EMIT("vadd.f32    q4, q4, q14")                   /* q4   = ai1 + ci1 */ \
-    __ASM_EMIT("vstm        %[dst_re]!, {q0-q1}") \
-    __ASM_EMIT("vstm        %[dst_im]!, {q4-q5}") \
-    __ASM_EMIT("4:")
 
     void packed_direct_butterfly_rank3(float *dst, size_t blocks) {
         ARCH_ARM_ASM(
