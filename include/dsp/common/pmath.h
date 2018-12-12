d06429306c57ae38e20887c05de841bd56854402
sadko4u <sadko4u@gmail.com>
2018-12-12 19:13:03 +0300

    Added native power functions for future optimizations.

--- include/dsp/common/pmath.h
+++ include/dsp/common/pmath.h
@@ -319,6 +319,57 @@ namespace dsp
      */
     extern void (* logd2)(float *dst, const float *src, size_t count);
 
+    /**
+     * Compute v[i] = c ^ v[i], the value to be raised should be non-negative
+     * @param v power array
+     * @param c value to be raised
+     * @param count number of elements in array
+     */
+    extern void (* powcv1)(float *v, float c, size_t count);
+
+    /**
+     * Compute dst[i] = c ^ v[i], the value to be raised should be non-negative
+     * @param dst output array
+     * @param v power array
+     * @param c value to be raised
+     * @param count number of elements in array
+     */
+    extern void (* powcv2)(float *dst, const float *v, float c, size_t count);
+
+    /**
+     * Compute v[i] = v[i] ^ c, the value to be raised should be non-negative
+     * @param v values to be raised
+     * @param c power value
+     * @param count number of elements in array
+     */
+    extern void (* powvc1)(float *c, float v, size_t count);
+
+    /**
+     * Compute dst[i] = v[i] ^ c, the value to be raised should be non-negative
+     * @param dst output array
+     * @param v values to be raised
+     * @param c power value
+     * @param count number of elements in array
+     */
+    extern void (* powvc2)(float *dst, const float *c, float v, size_t count);
+
+    /**
+     * Compute v[i] = v[i] ^ x[i], the value to be raised should be non-negative
+     * @param v values to be raised
+     * @param x power values
+     * @param count number of elements in array
+     */
+    extern void (* powvx1)(float *v, const float *x, size_t count);
+
+    /**
+     * Compute dst[i] = v[i] ^ x[i], the value to be raised should be non-negative
+     * @param dst output array
+     * @param v values to be raised
+     * @param x power values
+     * @param count number of elements in array
+     */
+    extern void (* powvx2)(float *dst, const float *v, const float *x, size_t count);
+
 }
 
 #endif /* DSP_COMMON_PMATH_H_ */
