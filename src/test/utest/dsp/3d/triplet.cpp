c4a2a8c3bb2258476a53913ebcfc638700a9a949
sadko4u <sadko4u@gmail.com>
2018-12-31 18:34:41 +0300

    Updated test

--- src/test/utest/dsp/3d/triplet.cpp
+++ src/test/utest/dsp/3d/triplet.cpp
@@ -90,13 +90,13 @@ UTEST_BEGIN("dsp.3d", triplet)
             dsp::init_vector_p2(&v[1], &t->p[1], &t->p[2]);
             dsp::init_vector_p2(&v[3], &t->p[2], &t->p[0]);
 
-            UTEST_ASSERT(check_triplet3d_v2n(&v[0], &v[1], &n1) >= 0);
-            UTEST_ASSERT(check_triplet3d_v2n(&v[1], &v[2], &n1) >= 0);
-            UTEST_ASSERT(check_triplet3d_v2n(&v[2], &v[0], &n1) >= 0);
+            UTEST_ASSERT(check_triplet3d_v2n(&v[0], &v[1], &n1) >= 0.0f);
+            UTEST_ASSERT(check_triplet3d_v2n(&v[1], &v[2], &n1) >= 0.0f);
+            UTEST_ASSERT(check_triplet3d_v2n(&v[2], &v[0], &n1) >= 0.0f);
 
-            UTEST_ASSERT(check_triplet3d_v2n(&v[0], &v[1], &n2) <= 0);
-            UTEST_ASSERT(check_triplet3d_v2n(&v[1], &v[2], &n2) <= 0);
-            UTEST_ASSERT(check_triplet3d_v2n(&v[2], &v[0], &n2) <= 0);
+            UTEST_ASSERT(check_triplet3d_v2n(&v[0], &v[1], &n2) <= 0.0f);
+            UTEST_ASSERT(check_triplet3d_v2n(&v[1], &v[2], &n2) <= 0.0f);
+            UTEST_ASSERT(check_triplet3d_v2n(&v[2], &v[0], &n2) <= 0.0f);
 
             dsp::init_vector(&v[2], &n1);
             UTEST_ASSERT(check_triplet3d_vv(v) >= 0);
