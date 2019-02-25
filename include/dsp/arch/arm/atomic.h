f6af1a13ca9c98752f88d2a45a56803f03383100
sadko4u <sadko4u@gmail.com>
2019-02-25 20:33:50 +0300

    Fixing regressions for ARM

--- include/dsp/arch/arm/atomic.h
+++ include/dsp/arch/arm/atomic.h
@@ -8,8 +8,8 @@
 #ifndef DSP_ARCH_ARM_ATOMIC_H_
 #define DSP_ARCH_ARM_ATOMIC_H_
 
-#define ATOMIC_CAS_DEF(type, qsz)                      \
-    inline type arm_compare_and_swap(type *ptr, type exp, type rep) \
+#define ATOMIC_CAS_DEF(type, qsz, extra)                        \
+    inline type atomic_cas(extra type *ptr, type exp, type rep) \
     { \
         type tmp; \
         \
@@ -26,33 +26,6 @@
             : "cc", "memory" \
         ); \
         return tmp; \
-    }
-
-ATOMIC_CAS_DEF(int8_t, "exb")
-ATOMIC_CAS_DEF(uint8_t, "exb")
-ATOMIC_CAS_DEF(int16_t, "exh")
-ATOMIC_CAS_DEF(uint16_t, "exh")
-ATOMIC_CAS_DEF(int32_t, "ex")
-ATOMIC_CAS_DEF(uint32_t, "ex")
-
-#undef ATOMIC_CAS_DEF
-
-#define ATOMIC_CAS_DEF(type, qsz, extra)                        \
-    inline bool atomic_cas(extra type *ptr, type exp, type rep) \
-    { \
-        type tmp; \
-        \
-        ARCH_ARM_ASM \
-        ( \
-            __ASM_EMIT("dmb") \
-            __ASM_EMIT("ldr" qsz "      %[tmp], [%[ptr]]") \
-            __ASM_EMIT("teq             %[tmp], %[exp]") \
-            __ASM_EMIT("str" qsz "eq    %[tmp], %[rep], [%[ptr]]") \
-            : [tmp] "=&r" (tmp) \
-            : [ptr] "r" (ptr), [exp] "r" (exp), [rep] "r" (rep) \
-            : "cc", "memory" \
-        ); \
-        return bool(tmp); \
     }
 
 ATOMIC_CAS_DEF(int8_t, "exb", )
@@ -149,8 +122,8 @@ ATOMIC_SWAP_DEF(uint32_t, "ex", volatile)
 //-----------------------------------------------------------------------------
 // Atomic operations
 #define atomic_init(lk)         lk = 1
-#define atomic_trylock(lk)      arm_compare_and_swap(&lk, 1, 0)
-#define atomic_unlock(lk)       arm_compare_and_swap(&lk, 0, 1)
+#define atomic_trylock(lk)      atomic_cas(&lk, 1, 0)
+#define atomic_unlock(lk)       atomic_cas(&lk, 0, 1)
 
 
 #endif /* DSP_ARCH_ARM_ATOMIC_H_ */
