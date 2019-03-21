800f67878cc1860be417d1c91e768049af548fbd
sadko4u <sadko4u@gmail.com>
2019-03-22 00:23:11 +0300

    Made variables optional

--- scripts/make/tools.mk
+++ scripts/make/tools.mk
@@ -1,12 +1,12 @@
-export CC               = gcc
-export CXX              = g++
-export PHP              = php
-export LD               = ld
+export CC              ?= gcc
+export CXX             ?= g++
+export PHP             ?= php
+export LD              ?= ld
 
 export MAKE_OPTS        = -s
 export CFLAGS          += $(CC_ARCH) -std=c++98 -fdata-sections -pthread -ffunction-sections -fno-exceptions -fno-asynchronous-unwind-tables -Wall -pipe -fno-rtti $(CC_FLAGS) -DLSP_MAIN_VERSION=\"$(VERSION)\" -DLSP_INSTALL_PREFIX=\"$(PREFIX)\"
 export CXXFLAGS		   += $(CC_ARCH) -std=c++98 -fdata-sections -pthread -ffunction-sections -fno-exceptions -fno-asynchronous-unwind-tables -Wall -pipe -fno-rtti $(CC_FLAGS) -DLSP_MAIN_VERSION=\"$(VERSION)\" -DLSP_INSTALL_PREFIX=\"$(PREFIX)\"
-export LDFLAGS          = $(LD_ARCH) -L$(LD_PATH)
+export LDFLAGS         += $(LD_ARCH) -L$(LD_PATH)
 export SO_FLAGS         = $(CC_ARCH) -Wl,-rpath,$(LD_PATH) -Wl,-z,relro,-z,now -Wl,--gc-sections -shared -Llibrary -lc -fPIC
 export MERGE_FLAGS      = $(LD_ARCH) -r
 export EXE_TEST_FLAGS   = $(CC_ARCH) -Wl,-rpath,$(LD_PATH)
