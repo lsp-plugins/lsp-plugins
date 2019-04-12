cc233adad8b18a071400924d637ad08381694372
sadko4u <sadko4u@gmail.com>
2019-04-12 11:08:35 +0300

    Updated unit test

--- src/test/utest/dsp/copy/copy.cpp
+++ src/test/utest/dsp/copy/copy.cpp
@@ -58,11 +58,9 @@ IF_ARCH_AARCH64(
 typedef void (* copy_t)(float *dst, const float *src, size_t count);
 
 UTEST_BEGIN("dsp.copy", copy)
-    void call(const char *label, size_t align, copy_t func1, copy_t func2)
+    void callc(const char *label, size_t align, copy_t func)
     {
-        if (!UTEST_SUPPORTED(func1))
-            return;
-        if (!UTEST_SUPPORTED(func2))
+        if (!UTEST_SUPPORTED(func))
             return;
 
         UTEST_FOREACH(count, 0, 1, 3, 4, 5, 8, 16, 24, 32, 33, 64, 47, 0x80, 0x100, 0x1ff, 999, 0xfff, 0x1fff)
@@ -74,8 +72,8 @@ UTEST_BEGIN("dsp.copy", copy)
                 FloatBuffer dst2(dst1);
 
                 printf("Testing %s, of %d samples, mask=%x\n", label, int(count), int(mask));
-                func1(dst1, src, count);
-                func2(dst2, src, count);
+                native::copy(dst1, src, count);
+                func(dst2, src, count);
 
                 UTEST_ASSERT_MSG(dst1.valid(), "Destination buffer 1 corrupted");
                 UTEST_ASSERT_MSG(dst2.valid(), "Destination buffer 2 corrupted");
@@ -89,8 +87,53 @@ UTEST_BEGIN("dsp.copy", copy)
 
                 printf("Testing %s, of %d samples from %d offset, mask=%x\n", label, int(count >> 1), int(count >> 2), int(mask));
                 float *dptr1 = dst1, *dptr2 = dst2;
-                func1(dptr1, &dptr1[count >> 2], count >> 1);
-                func2(dptr2, &dptr2[count >> 2], count >> 1);
+                native::copy(dptr1, &dptr1[count >> 2], count >> 1);
+                func(dptr2, &dptr2[count >> 2], count >> 1);
+
+                UTEST_ASSERT_MSG(dst1.valid(), "Destination buffer 1 corrupted");
+                UTEST_ASSERT_MSG(dst2.valid(), "Destination buffer 2 corrupted");
+
+                if (!dst1.equals_absolute(dst2))
+                {
+                    dst1.dump("dst1");
+                    dst2.dump("dst2");
+                    UTEST_FAIL_MSG("Output of functions for test '%s' differs", label);
+                }
+            }
+        }
+    }
+
+    void callm(const char *label, size_t align, copy_t func)
+    {
+        if (!UTEST_SUPPORTED(func))
+            return;
+
+        UTEST_FOREACH(count, 0, 1, 3, 4, 5, 8, 16, 24, 32, 33, 64, 47, 0x80, 0x100, 0x1ff, 999, 0xfff, 0x1fff)
+        {
+            for (size_t mask=0; mask <= 0x03; ++mask)
+            {
+                FloatBuffer src(count, align, mask & 0x01);
+                FloatBuffer dst1(count, align, mask & 0x02);
+                FloatBuffer dst2(dst1);
+
+                printf("Testing %s, of %d samples, mask=%x\n", label, int(count), int(mask));
+                native::move(dst1, src, count);
+                func(dst2, src, count);
+
+                UTEST_ASSERT_MSG(dst1.valid(), "Destination buffer 1 corrupted");
+                UTEST_ASSERT_MSG(dst2.valid(), "Destination buffer 2 corrupted");
+
+                if (!dst1.equals_absolute(dst2))
+                {
+                    dst1.dump("dst1");
+                    dst2.dump("dst2");
+                    UTEST_FAIL_MSG("Output of functions for test '%s' differs", label);
+                }
+
+                printf("Testing %s, of %d samples from %d offset, mask=%x\n", label, int(count >> 1), int(count >> 2), int(mask));
+                float *dptr1 = dst1, *dptr2 = dst2;
+                native::move(&dptr1[count >> 2], dptr1, count >> 1);
+                func(&dptr2[count >> 2], dptr2, count >> 1);
 
                 UTEST_ASSERT_MSG(dst1.valid(), "Destination buffer 1 corrupted");
                 UTEST_ASSERT_MSG(dst2.valid(), "Destination buffer 2 corrupted");
@@ -107,18 +150,18 @@ UTEST_BEGIN("dsp.copy", copy)
 
     UTEST_MAIN
     {
-        IF_ARCH_X86(call("x86::copy", 16, native::copy, x86::copy));
-        IF_ARCH_X86(call("sse::copy", 16, native::copy, sse::copy));
-        IF_ARCH_X86(call("sse::copy_movntps", 16, native::copy, sse::copy_movntps));
-        IF_ARCH_X86(call("sse::move", 16, native::move, sse::move));
-        IF_ARCH_X86(call("sse3::copy", 16, native::copy, sse3::copy));
-        IF_ARCH_X86(call("avx::copy", 16, native::copy, avx::copy));
+        IF_ARCH_X86(callc("x86::copy", 16, x86::copy));
+        IF_ARCH_X86(callc("sse::copy", 16, sse::copy));
+        IF_ARCH_X86(callc("sse::copy_movntps", 16, sse::copy_movntps));
+        IF_ARCH_X86(callc("sse3::copy", 16, sse3::copy));
+        IF_ARCH_X86(callc("avx::copy", 16, avx::copy));
+        IF_ARCH_X86(callm("sse::move", 16, sse::move));
 
-        IF_ARCH_ARM(call("neon_d32::copy", 16, native::move, neon_d32::copy));
-        IF_ARCH_ARM(call("neon_d32::move", 16, native::move, neon_d32::move));
+        IF_ARCH_ARM(callc("neon_d32::copy", 16, neon_d32::copy));
+        IF_ARCH_ARM(callm("neon_d32::move", 16, neon_d32::move));
 
-        IF_ARCH_AARCH64(call("asimd::copy", 16, native::move, asimd::copy));
-        IF_ARCH_AARCH64(call("asimd::move", 16, native::move, asimd::move));
+        IF_ARCH_AARCH64(callc("asimd::copy", 16, asimd::copy));
+        IF_ARCH_AARCH64(callm("asimd::move", 16, asimd::move));
     }
 
 UTEST_END;
