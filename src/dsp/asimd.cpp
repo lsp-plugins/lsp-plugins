23f7160ad3e273f7c3b5b23771e7b01e46cd2a37
sadko4u <sadko4u@gmail.com>
2019-04-22 13:34:46 +0300

    Updated asimd module

--- src/dsp/asimd.cpp
+++ src/dsp/asimd.cpp
@@ -48,6 +48,7 @@ namespace asimd // TODO: make constants common for all architectures
 
 #include <dsp/arch/aarch64/asimd/copy.h>
 #include <dsp/arch/aarch64/asimd/complex.h>
+#include <dsp/arch/aarch64/asimd/pcomplex.h>
 
 #define EXPORT2(function, export)           dsp::function = asimd::export; TEST_EXPORT(asimd::export);
 #define EXPORT1(function)                   EXPORT2(function, function)
@@ -67,13 +68,14 @@ namespace asimd
 
         EXPORT1(complex_mul2);
         EXPORT1(complex_mul3);
-
         EXPORT1(complex_div2);
         EXPORT1(complex_rdiv2);
         EXPORT1(complex_div3);
         EXPORT1(complex_mod);
         EXPORT1(complex_rcp1);
         EXPORT1(complex_rcp2);
+
+        EXPORT1(pcomplex_mul2);
     }
 }
 
