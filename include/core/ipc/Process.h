719cb03765667d55d0b69a1cde18359bbe4506ae
sadko4u <sadko4u@gmail.com>
2019-07-25 20:17:59 +0300

    Implemented common set of functions.
    Need to implement platform-specific functions.

--- include/core/ipc/Process.h
+++ include/core/ipc/Process.h
@@ -12,7 +12,6 @@
 #include <unistd.h>
 #include <core/LSPString.h>
 #include <data/cvector.h>
-#include <data/cstorage.h>
 
 #ifdef PLATFORM_WINDOWS
     #include <processthreadsapi.h>
@@ -37,31 +36,26 @@ namespace lsp
                     PSTATUS_EXITED
                 };
 
-#ifdef PLATFORM_WINDOWS
                 typedef struct envvar_t {
-                    WCHAR      *name;
-                    WCHAR      *value;
+                    LSPString   name;
+                    LSPString   value;
                 } envvar_t;
-#else
-                typedef struct envvar_t {
-                    char       *name;
-                    char       *value;
-                };
-#endif
+
             private:
-#ifdef PLATFORM_WINDOWS
-                WCHAR                  *sCommand;
-                cvector<WCHAR>          vArgs;
-                cstorage<envvar_t>      vEnv;
-                PROCESS_INFORMATION     sPI;
-#else
-                char                   *sCommand;
-                cvector<char>           vArgs;
-                cstorage<envvar_t>      vEnv;
-                pid_t                   nPID;
-#endif
+                LSPString               sCommand;
+                cvector<LSPString>      vArgs;
+                cvector<envvar_t>       vEnv;
                 pstatus_t               nStatus;
                 int                     nExitCode;
+
+#ifdef PLATFORM_WINDOWS
+                PROCESS_INFORMATION     sPI;
+#else
+                pid_t                   nPID;
+#endif
+            protected:
+                static void     do_clear_args(cvector<LSPString> *args);
+                static void     do_clear_env(cvector<envvar_t> *env);
 
             public:
                 explicit Process();
@@ -87,56 +81,56 @@ namespace lsp
                  * Get overall number of additional command-line arguments
                  * @return number of additional command-line arguments
                  */
-                size_t      arguments() const;
+                size_t      args() const;
 
                 /**
                  * Add argument at the end of command line
                  * @param value argument value
                  * @return status of operation
                  */
-                status_t    add_argument(const LSPString *value);
+                status_t    add_arg(const LSPString *value);
 
                 /**
                  * Add argument at the end of command line
                  * @param value argument value in UTF-8 encoding
                  * @return status of operation
                  */
-                status_t    add_argument(const char *value);
+                status_t    add_arg(const char *value);
 
                 /**
                  * Set value of argument at the specified place
                  * @param value argument value
                  * @return status of operation
                  */
-                status_t    set_argument(size_t index, const LSPString *value);
+                status_t    set_arg(size_t index, const LSPString *value);
 
                 /**
                  * Set value of argument at the specified place
                  * @param value argument value in UTF-8 encoding
                  * @return status of operation
                  */
-                status_t    set_argument(size_t index, const char *value);
+                status_t    set_arg(size_t index, const char *value);
 
                 /**
                  * Get value of argument at the specified place
                  * @param value pointer to store argument value
                  * @return status of operation
                  */
-                status_t    get_argument(size_t index, LSPString *value);
+                status_t    get_arg(size_t index, LSPString *value);
 
                 /**
                  * Get value of argument at the specified place
                  * @param value pointer to store argument value
                  * @return status of operation
                  */
-                status_t    get_argument(size_t index, char **value);
+                status_t    get_arg(size_t index, char **value);
 
                 /**
                  * Remove the argument at the specified place
                  * @param value pointer to store value of removed argument
                  * @return status of operation
                  */
-                status_t    remove_argument(size_t index, LSPString *value = NULL);
+                status_t    remove_arg(size_t index, LSPString *value = NULL);
 
                 /**
                  * Remove the argument at the specified place
@@ -144,28 +138,34 @@ namespace lsp
                  *        The pointer should be free()'d after use
                  * @return status of operation
                  */
-                status_t    remove_argument(size_t index, char **value = NULL);
+                status_t    remove_arg(size_t index, char **value = NULL);
 
                 /**
                  * Insert argument at the specified position
                  * @param value argument value
                  * @return status of operation
                  */
-                status_t    insert_argument(size_t index, const LSPString *value);
+                status_t    insert_arg(size_t index, const LSPString *value);
 
                 /**
                  * Insert argument at the specified position
                  * @param value argument value in UTF-8 encoding
                  * @return status of operation
                  */
-                status_t    insert_argument(size_t index, const char *value);
+                status_t    insert_arg(size_t index, const char *value);
+
+                /**
+                 * Clear arguments;
+                 * @return status of operation
+                 */
+                status_t    clear_args();
 
             public:
                 /**
-                 * Copy environment variables of the current process
-                 * @return status of operation
+                 * Return number of environment variables
+                 * @return number of environment variables
                  */
-                status_t    copy_env();
+                status_t    envs() const;
 
                 /**
                  * Set value of the specific environment variable
@@ -217,7 +217,39 @@ namespace lsp
                  */
                 status_t    get_env(const char *key, char **value = NULL);
 
+                /**
+                 * Obtain the key and value of environment variable by index
+                 * @param idx the environment variable index
+                 * @param key environment variable key
+                 * @param value environment variable value
+                 * @return status of operation
+                 */
+                status_t    read_env(size_t idx, LSPString *key = NULL, LSPString *value = NULL);
+
+                /**
+                 * Obtain the value of the specific environment variable
+                 * @param idx the environment variable index
+                 * @param key the name of environment variable in UTF-8 encoding
+                 *        The obtained pointer should be free()'d after use
+                 * @param pointer to store value of the environment variable in UTF-8 encoding.
+                 *        The obtained pointer should be free()'d after use
+                 * @return status of operation
+                 */
+                status_t    read_env(size_t idx, char **key = NULL, char **value = NULL);
+
+                /**
+                 * Clear all environment variables
+                 * @return status of operation
+                 */
+                status_t    clear_env();
+
             public:
+                /**
+                 * Copy environment variables of the current process
+                 * @return status of operation
+                 */
+                status_t    copy_env();
+
                 /**
                  * Launch the process
                  * @return status of operation
@@ -238,17 +270,17 @@ namespace lsp
 
                 /**
                  * Wait for the process termination
-                 * @param millis number of milliseconds to wait
+                 * @param millis number of milliseconds to wait, negative value means infinite wait
                  * @return status of operation
                  */
-                status_t    wait(size_t millis);
+                status_t    wait(wsize_t millis);
 
                 /**
                  * Get process exit status
                  * @param code pointer to save exit status
                  * @return status of operation
                  */
-                status_t    exit_code(int *code) const;
+                status_t    exit_code(int *code);
         };
     
     } /* namespace ipc */
--- src/core/ipc/Process.cpp
+++ src/core/ipc/Process.cpp
@@ -14,14 +14,591 @@ namespace lsp
         
         Process::Process()
         {
-            // TODO Auto-generated constructor stub
-            
+            nStatus             = PSTATUS_CREATED;
+            nExitCode           = 0;
+
+#ifdef PLATFORM_WINDOWS
+            sPI.hProcess        = 0;
+            sPI.hThread         = 0;
+            sPI.dwProcessId     = 0;
+            sPI.dwThreadId      = 0;
+#else
+            nPID                = 0;
+#endif
         }
         
         Process::~Process()
         {
-            // TODO Auto-generated destructor stub
+            do_clear_args(&vArgs);
+            do_clear_env(&vEnv);
         }
     
+        void Process::do_clear_args(cvector<LSPString> *args)
+        {
+            for (size_t i=0, n=args->size(); i<n; ++i)
+            {
+                LSPString *arg = args->at(i);
+                delete arg;
+            }
+            args->flush();
+        }
+
+        void Process::do_clear_env(cvector<envvar_t> *env)
+        {
+            for (size_t i=0, n=env->size(); i<n; ++i)
+            {
+                envvar_t *var= env->at(i);
+                delete var;
+            }
+            env->flush();
+        }
+
+        status_t Process::set_command(const LSPString *cmd)
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+
+            if (cmd == NULL)
+            {
+                sCommand.clear();
+                return STATUS_OK;
+            }
+
+            return (sCommand.set(cmd)) ? STATUS_OK : STATUS_NO_MEM;
+        }
+
+        status_t Process::set_command(const char *cmd)
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+
+            if (cmd == NULL)
+            {
+                sCommand.clear();
+                return STATUS_OK;
+            }
+
+            return (sCommand.set_utf8(cmd)) ? STATUS_OK : STATUS_NO_MEM;
+        }
+
+        size_t Process::args() const
+        {
+            return vArgs.size();
+        }
+
+        status_t Process::add_arg(const LSPString *value)
+        {
+            if (value == NULL)
+                return STATUS_BAD_ARGUMENTS;
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+
+            LSPString *arg = new LSPString();
+            if (arg == NULL)
+                return STATUS_NO_MEM;
+
+            if (!arg->set(value))
+            {
+                delete arg;
+                return STATUS_NO_MEM;
+            }
+            if (!vArgs.add(arg))
+            {
+                delete arg;
+                return STATUS_NO_MEM;
+            }
+
+            return STATUS_OK;
+        }
+
+        status_t Process::add_arg(const char *value)
+        {
+            if (value == NULL)
+                return STATUS_BAD_ARGUMENTS;
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+
+            LSPString *arg = new LSPString();
+            if (arg == NULL)
+                return STATUS_NO_MEM;
+
+            if (!arg->set_utf8(value))
+            {
+                delete arg;
+                return STATUS_NO_MEM;
+            }
+            if (!vArgs.add(arg))
+            {
+                delete arg;
+                return STATUS_NO_MEM;
+            }
+
+            return STATUS_OK;
+        }
+
+        status_t Process::set_arg(size_t index, const LSPString *value)
+        {
+            if (value == NULL)
+                return STATUS_BAD_ARGUMENTS;
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+
+            LSPString *ptr = vArgs.get(index);
+            if (ptr == NULL)
+                return STATUS_BAD_ARGUMENTS;
+            return (ptr->set(value)) ? STATUS_OK : STATUS_NO_MEM;
+        }
+
+        status_t Process::set_arg(size_t index, const char *value)
+        {
+            if (value == NULL)
+                return STATUS_BAD_ARGUMENTS;
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+
+            LSPString *ptr = vArgs.get(index);
+            if (ptr == NULL)
+                return STATUS_BAD_ARGUMENTS;
+            return (ptr->set_utf8(value)) ? STATUS_OK : STATUS_NO_MEM;
+        }
+
+        status_t Process::get_arg(size_t index, LSPString *value)
+        {
+            LSPString *ptr = vArgs.get(index);
+            if (ptr == NULL)
+                return STATUS_BAD_ARGUMENTS;
+            if (value == NULL)
+                return STATUS_OK;
+            return (value->set(ptr)) ? STATUS_OK : STATUS_NO_MEM;
+        }
+
+        status_t Process::get_arg(size_t index, char **value)
+        {
+            LSPString *ptr = vArgs.get(index);
+            if (ptr == NULL)
+                return STATUS_BAD_ARGUMENTS;
+            if (value == NULL)
+                return STATUS_OK;
+
+            const char *p = ptr->get_utf8();
+            if (p == NULL)
+                return STATUS_NO_MEM;
+            char *dup   = ::strdup(p);
+            if (dup == NULL)
+                return STATUS_NO_MEM;
+
+            *value      = dup;
+            return STATUS_OK;
+        }
+
+        status_t Process::remove_arg(size_t index, LSPString *value)
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+
+            LSPString *ptr = vArgs.get(index);
+            if (ptr == NULL)
+                return STATUS_BAD_ARGUMENTS;
+            if (value != NULL)
+                value->swap(ptr);
+
+            vArgs.remove(index);
+            delete ptr;
+            return STATUS_OK;
+        }
+
+        status_t Process::remove_arg(size_t index, char **value)
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+
+            LSPString *ptr = vArgs.get(index);
+            if (ptr == NULL)
+                return STATUS_BAD_ARGUMENTS;
+            if (value != NULL)
+            {
+                const char *p = ptr->get_utf8();
+                if (p == NULL)
+                    return STATUS_NO_MEM;
+                char *dup   = ::strdup(p);
+                if (dup == NULL)
+                    return STATUS_NO_MEM;
+
+                *value = dup;
+            }
+
+            vArgs.remove(index);
+            delete ptr;
+            return STATUS_OK;
+        }
+
+        status_t Process::insert_arg(size_t index, const LSPString *value)
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+            if (value == NULL)
+                return STATUS_BAD_ARGUMENTS;
+
+            LSPString *arg = new LSPString();
+            if (arg == NULL)
+                return STATUS_NO_MEM;
+
+            if (!arg->set(value))
+            {
+                delete arg;
+                return STATUS_NO_MEM;
+            }
+            if (!vArgs.insert(arg, index))
+            {
+                delete arg;
+                return STATUS_NO_MEM;
+            }
+
+            return STATUS_OK;
+        }
+
+        status_t Process::insert_arg(size_t index, const char *value)
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+            if (value == NULL)
+                return STATUS_BAD_ARGUMENTS;
+
+            LSPString *arg = new LSPString();
+            if (arg == NULL)
+                return STATUS_NO_MEM;
+
+            if (!arg->set_utf8(value))
+            {
+                delete arg;
+                return STATUS_NO_MEM;
+            }
+            if (!vArgs.insert(arg, index))
+            {
+                delete arg;
+                return STATUS_NO_MEM;
+            }
+
+            return STATUS_OK;
+        }
+
+        status_t Process::clear_args()
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+            do_clear_args(&vArgs);
+            return STATUS_OK;
+        }
+
+        status_t Process::envs() const
+        {
+            return vEnv.size();
+        }
+
+        status_t Process::set_env(const LSPString *key, const LSPString *value)
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+            if ((key == NULL) || (value == NULL))
+                return STATUS_BAD_ARGUMENTS;
+
+            envvar_t *var;
+            for (size_t i=0, n=vEnv.size(); i<n; ++i)
+            {
+                var = vEnv.at(i);
+                if (var->name.equals(key))
+                    return (var->value.set(value)) ? STATUS_OK : STATUS_NO_MEM;
+            }
+
+            if ((var = new envvar_t) == NULL)
+                return STATUS_NO_MEM;
+
+            if ((!var->name.set(key)) || (!var->value.set(key)))
+            {
+                delete var;
+                return STATUS_NO_MEM;
+            }
+            if (!vEnv.add(var))
+            {
+                delete var;
+                return STATUS_NO_MEM;
+            }
+            return STATUS_OK;
+        }
+
+        status_t Process::set_env(const char *key, const char *value)
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+            if ((key == NULL) || (value == NULL))
+                return STATUS_BAD_ARGUMENTS;
+            LSPString k, v;
+            if ((!k.set_utf8(key)) || (!v.set_utf8(value)))
+                return STATUS_NO_MEM;
+            return set_env(&k, &v);
+        }
+
+        status_t Process::remove_env(const LSPString *key, LSPString *value)
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+            if (key == NULL)
+                return STATUS_BAD_ARGUMENTS;
+
+            for (size_t i=0, n=vEnv.size(); i<n; ++i)
+            {
+                envvar_t *var = vEnv.at(i);
+                if (var->name.equals(key))
+                {
+                    if (value != NULL)
+                        value->swap(&var->value);
+                    delete var;
+                    vEnv.remove(i, true);
+                    return STATUS_OK;
+                }
+            }
+
+            return STATUS_NOT_FOUND;
+        }
+
+        status_t Process::remove_env(const char *key, char **value)
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+            if (key == NULL)
+                return STATUS_BAD_ARGUMENTS;
+
+            LSPString k;
+            if (!k.set_utf8(key))
+                return STATUS_NO_MEM;
+
+            for (size_t i=0, n=vEnv.size(); i<n; ++i)
+            {
+                envvar_t *var = vEnv.at(i);
+                if (var->name.equals(&k))
+                {
+                    if (value != NULL)
+                    {
+                        const char *v   = var->value.get_utf8();
+                        if (v == NULL)
+                            return STATUS_NO_MEM;
+                        char *dup       = ::strdup(v);
+                        if (dup == NULL)
+                            return STATUS_NO_MEM;
+                        *value          = dup;
+                    }
+                    delete var;
+                    vEnv.remove(i, true);
+                    return STATUS_OK;
+                }
+            }
+
+            return STATUS_NOT_FOUND;
+        }
+
+        status_t Process::get_env(const LSPString *key, LSPString *value)
+        {
+            if (key == NULL)
+                return STATUS_BAD_ARGUMENTS;
+
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+            if (key == NULL)
+                return STATUS_BAD_ARGUMENTS;
+
+            for (size_t i=0, n=vEnv.size(); i<n; ++i)
+            {
+                envvar_t *var = vEnv.at(i);
+                if (var->name.equals(key))
+                {
+                    if (value != NULL)
+                    {
+                        if (!value->set(&var->value))
+                            return STATUS_NO_MEM;
+                    }
+                    return STATUS_OK;
+                }
+            }
+
+            return STATUS_NOT_FOUND;
+        }
+
+        status_t Process::get_env(const char *key, char **value)
+        {
+            if (key == NULL)
+                return STATUS_BAD_ARGUMENTS;
+
+            LSPString k;
+            if (!k.set_utf8(key))
+                return STATUS_NO_MEM;
+
+            for (size_t i=0, n=vEnv.size(); i<n; ++i)
+            {
+                envvar_t *var = vEnv.at(i);
+                if (var->name.equals(&k))
+                {
+                    if (value != NULL)
+                    {
+                        const char *v   = var->value.get_utf8();
+                        if (v == NULL)
+                            return STATUS_NO_MEM;
+                        char *dup       = ::strdup(v);
+                        if (dup == NULL)
+                            return STATUS_NO_MEM;
+                        *value          = dup;
+                    }
+                    return STATUS_OK;
+                }
+            }
+
+            return STATUS_NOT_FOUND;
+        }
+
+        status_t Process::read_env(size_t idx, LSPString *key, LSPString *value)
+        {
+            if ((key == NULL) && (value == NULL))
+                return STATUS_BAD_ARGUMENTS;
+
+            envvar_t *var = vEnv.get(idx);
+            if (var == NULL)
+                return STATUS_BAD_ARGUMENTS;
+
+            LSPString tk, tv;
+            if (key != NULL)
+            {
+                if (!tk.set(&var->name))
+                    return STATUS_NO_MEM;
+                if (value != NULL)
+                {
+                    if (!tv.set(&var->value))
+                        return STATUS_NO_MEM;
+                    value->swap(&tv);
+                }
+
+                key->swap(&tk);
+                return STATUS_OK;
+            }
+
+            if (!tv.set(&var->value))
+                return STATUS_NO_MEM;
+
+            value->swap(&tv);
+            return STATUS_OK;
+        }
+
+        status_t Process::read_env(size_t idx, char **key, char **value)
+        {
+            if ((key == NULL) && (value == NULL))
+                return STATUS_BAD_ARGUMENTS;
+
+            envvar_t *var = vEnv.get(idx);
+            if (var == NULL)
+                return STATUS_BAD_ARGUMENTS;
+
+            char *tk, *tv;
+            const char *s;
+
+            if (key != NULL)
+            {
+                s = var->name.get_utf8();
+                tk = (s != NULL) ? ::strdup(s) : NULL;
+                if (tk == NULL)
+                    return STATUS_NO_MEM;
+
+                if (value != NULL)
+                {
+                    s = var->value.get_utf8();
+                    tv = (s != NULL) ? ::strdup(s) : NULL;
+                    if (tv == NULL)
+                    {
+                        ::free(tk);
+                        return STATUS_NO_MEM;
+                    }
+
+                    *value = tv;
+                }
+
+                *key = tk;
+                return STATUS_OK;
+            }
+
+            s = var->value.get_utf8();
+            tv = (s != NULL) ? ::strdup(s) : NULL;
+            if (tv == NULL)
+                return STATUS_NO_MEM;
+
+            *value = tv;
+            return STATUS_OK;
+        }
+
+        status_t Process::clear_env()
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+            do_clear_env(&vEnv);
+            return STATUS_OK;
+        }
+
+        bool Process::exited()
+        {
+            if (nStatus == PSTATUS_EXITED)
+                return true;
+            else if (nStatus == PSTATUS_CREATED)
+                return false;
+            status_t res = wait(0);
+            if (res != STATUS_OK)
+                return false;
+            return nStatus == PSTATUS_EXITED;
+        }
+
+        bool Process::running()
+        {
+            if (nStatus != PSTATUS_RUNNING)
+                return false;
+            status_t res = wait(0);
+            if (res != STATUS_OK)
+                return true;
+            return nStatus == PSTATUS_RUNNING;
+        }
+
+        status_t Process::exit_code(int *code)
+        {
+            if (code == NULL)
+                return STATUS_BAD_ARGUMENTS;
+
+            if (nStatus == PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+            if (nStatus == PSTATUS_RUNNING)
+            {
+                status_t res = wait(0);
+                if (res != STATUS_OK)
+                    return STATUS_BAD_STATE;
+            }
+
+            *code   = nExitCode;
+            return STATUS_OK;
+        }
+
+        status_t Process::copy_env()
+        {
+            // TODO: platform-specific stuff
+            return STATUS_OK;
+        }
+
+        status_t Process::launch()
+        {
+            if (nStatus != PSTATUS_CREATED)
+                return STATUS_BAD_STATE;
+
+            // TODO: platform-specific stuff
+            return STATUS_OK;
+        }
+
+        status_t Process::wait(wsize_t millis)
+        {
+            // TODO: platform-specific stuff
+            return STATUS_OK;
+        }
+
     } /* namespace ipc */
 } /* namespace lsp */
