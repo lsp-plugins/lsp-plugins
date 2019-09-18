4849d0c013932d850b517f6cf5d495632726d8d4
sadko4u <sadko4u@gmail.com>
2019-09-18 23:46:04 +0300

    Updated evaluator header and stubs

--- src/core/calc/evaluator.cpp
+++ src/core/calc/evaluator.cpp
@@ -11,197 +11,197 @@ namespace lsp
 {
     namespace calc
     {
-        status_t eval_ternary(value_t *value, const expr_t *expr)
+        status_t eval_ternary(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_xor(value_t *value, const expr_t *expr)
+        status_t eval_xor(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_or(value_t *value, const expr_t *expr)
+        status_t eval_or(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_and(value_t *value, const expr_t *expr)
+        status_t eval_and(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_bit_xor(value_t *value, const expr_t *expr)
+        status_t eval_bit_xor(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_bit_or(value_t *value, const expr_t *expr)
+        status_t eval_bit_or(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_bit_and(value_t *value, const expr_t *expr)
+        status_t eval_bit_and(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_cmp(value_t *value, const expr_t *expr)
+        status_t eval_cmp(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_cmp_eq(value_t *value, const expr_t *expr)
+        status_t eval_cmp_eq(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_cmp_ne(value_t *value, const expr_t *expr)
+        status_t eval_cmp_ne(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_cmp_lt(value_t *value, const expr_t *expr)
+        status_t eval_cmp_lt(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_cmp_gt(value_t *value, const expr_t *expr)
+        status_t eval_cmp_gt(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_cmp_le(value_t *value, const expr_t *expr)
+        status_t eval_cmp_le(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_cmp_ge(value_t *value, const expr_t *expr)
+        status_t eval_cmp_ge(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_icmp(value_t *value, const expr_t *expr)
+        status_t eval_icmp(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_icmp_eq(value_t *value, const expr_t *expr)
+        status_t eval_icmp_eq(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_icmp_ne(value_t *value, const expr_t *expr)
+        status_t eval_icmp_ne(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_icmp_lt(value_t *value, const expr_t *expr)
+        status_t eval_icmp_lt(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_icmp_gt(value_t *value, const expr_t *expr)
+        status_t eval_icmp_gt(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_icmp_le(value_t *value, const expr_t *expr)
+        status_t eval_icmp_le(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_icmp_ge(value_t *value, const expr_t *expr)
+        status_t eval_icmp_ge(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_add(value_t *value, const expr_t *expr)
+        status_t eval_add(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_sub(value_t *value, const expr_t *expr)
+        status_t eval_sub(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_iadd(value_t *value, const expr_t *expr)
+        status_t eval_iadd(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_isub(value_t *value, const expr_t *expr)
+        status_t eval_isub(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_mul(value_t *value, const expr_t *expr)
+        status_t eval_mul(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_div(value_t *value, const expr_t *expr)
+        status_t eval_div(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_imul(value_t *value, const expr_t *expr)
+        status_t eval_imul(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_idiv(value_t *value, const expr_t *expr)
+        status_t eval_idiv(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_fmod(value_t *value, const expr_t *expr)
+        status_t eval_fmod(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_imod(value_t *value, const expr_t *expr)
+        status_t eval_imod(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_power(value_t *value, const expr_t *expr)
+        status_t eval_power(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_not(value_t *value, const expr_t *expr)
+        status_t eval_not(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_psign(value_t *value, const expr_t *expr)
+        status_t eval_psign(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_nsign(value_t *value, const expr_t *expr)
+        status_t eval_nsign(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_exists(value_t *value, const expr_t *expr)
+        status_t eval_exists(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_db(value_t *value, const expr_t *expr)
+        status_t eval_db(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_resolve(value_t *value, const expr_t *expr)
+        status_t eval_resolve(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
 
-        status_t eval_value(value_t *value, const expr_t *expr)
+        status_t eval_value(value_t *value, const expr_t *expr, eval_env_t *env)
         {
             return STATUS_NOT_IMPLEMENTED;
         }
