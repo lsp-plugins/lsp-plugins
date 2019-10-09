87cb231fce02cefcbd567bbe5ee08159f33ee123
sadko4u <sadko4u@gmail.com>
2019-10-09 19:12:05 +0300

    * Fixed corrupted LADSPA binaries due to lack of   objects containing non-required resources.   Replaced objects with empty stubs.

--- include/core/resource.h
+++ include/core/resource.h
@@ -43,4 +43,18 @@ namespace lsp
     ssize_t     resource_skip_bytes(const void **ptr, size_t count);
 }
 
+// This macro should be used for binaries that do not need builtin resources
+#define BUILTIN_RESOURCES_STUB \
+    namespace lsp \
+    { \
+        const resource_t builtin_resources[] = \
+        { \
+            { NULL, NULL, RESOURCE_UNKNOWN } \
+        }; \
+        \
+        const char *string_dictionary = ""; \
+        \
+        const float float_dictionary[] = { 0.0f }; \
+    }
+
 #endif /* CORE_RESOURCE_H_ */
