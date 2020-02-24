17bbd404acd5ba3e1c0c917d4277e336ed7e6027
sadko4u <sadko4u@gmail.com>
2020-02-24 21:26:21 +0300

    Refactoring of impulse responses/impulse reverb plugins

--- include/plugins/impulse_reverb.h
+++ include/plugins/impulse_reverb.h
@@ -13,6 +13,7 @@
 #include <core/util/Convolver.h>
 #include <core/util/Bypass.h>
 #include <core/util/Delay.h>
+#include <core/util/Toggle.h>
 #include <core/filters/Equalizer.h>
 #include <core/sampling/SamplePlayer.h>
 #include <core/files/AudioFile.h>
@@ -79,8 +80,9 @@ namespace lsp
                 AudioFile      *pCurr;          // Current audio file
                 AudioFile      *pSwap;          // Pointer to audio file for swapping between RT and non-RT code
 
+                Toggle          sListen;        // Listen toggle
                 Sample         *pSwapSample;
-                Sample         *pCurrSample;                                                    // Rendered file sample
+                Sample         *pCurrSample;    // Rendered file sample
                 float          *vThumbs[impulse_reverb_base_metadata::TRACKS_MAX];           // Thumbnails
                 float           fNorm;          // Norming factor
                 bool            bRender;        // Flag that indicates that file needs rendering
