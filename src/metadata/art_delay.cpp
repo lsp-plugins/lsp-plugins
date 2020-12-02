6f3330d877e8d0e49a29ba74ec3dd5aa44a39b9c
sadko4u <sadko4u@gmail.com>
2020-12-03 01:38:35 +0300

    Submitted draft for the Art Delay metadata

---
+++ src/metadata/art_delay.cpp
@@ -0,0 +1,241 @@
+/*
+ * Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-plugins
+ * Created on: 2 дек. 2020 г.
+ *
+ * lsp-plugins is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-plugins is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-plugins. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <metadata/plugins.h>
+#include <metadata/ports.h>
+#include <metadata/developers.h>
+
+namespace lsp
+{
+    static const port_item_t art_delay_lines[] =
+    {
+        { "0-3",            "art_delay.lines_0:3"   },
+        { "4-7",            "art_delay.lines_4:7"   },
+        { "8-11",           "art_delay.lines_8:11"  },
+        { "12-15",          "art_delay.lines_12:15" },
+        { NULL, NULL }
+    };
+
+    static port_item_t art_delay_maxlen[] =
+    {
+        { "1",              NULL    },
+        { "2",              NULL    },
+        { "4",              NULL    },
+        { "8",              NULL    },
+        { "16",             NULL    },
+        { "24",             NULL    },
+        { "32",             NULL    },
+        { "40",             NULL    },
+        { "48",             NULL    },
+        { "56",             NULL    },
+        { "64",             NULL    },
+
+        { NULL, NULL }
+    };
+
+    static port_item_t art_delay_references[] =
+    {
+        { "None",           "art_delay.none" },
+
+        { "0",              NULL    },
+        { "1",              NULL    },
+        { "2",              NULL    },
+        { "3",              NULL    },
+        { "4",              NULL    },
+        { "5",              NULL    },
+        { "6",              NULL    },
+        { "7",              NULL    },
+        { "8",              NULL    },
+        { "9",              NULL    },
+        { "10",             NULL    },
+        { "11",             NULL    },
+        { "12",             NULL    },
+        { "13",             NULL    },
+        { "14",             NULL    },
+        { "15",             NULL    },
+
+        { NULL, NULL }
+    };
+
+    static port_item_t art_delay_tempo[] =
+    {
+        { "None",           "art_delay.none" },
+
+        { "0",              NULL    },
+        { "1",              NULL    },
+        { "2",              NULL    },
+        { "3",              NULL    },
+        { "4",              NULL    },
+        { "5",              NULL    },
+        { "6",              NULL    },
+        { "7",              NULL    },
+        { "8",              NULL    },
+
+        { NULL, NULL }
+    };
+
+    #define ART_PAN_MONO(id, label) \
+        PAN_CTL("p" id, label " panorama", 0.0f)
+
+    #define ART_PAN_STEREO(id, label) \
+        PAN_CTL("pl" id, label " left channel panorama", -100.0f), \
+        PAN_CTL("pr" id, label " right channel panorama", 100.0f)
+
+    #define ART_DELAY_COMMON(pan)  \
+        BYPASS, \
+        COMBO("lsel", "Delay line selector", 0, art_delay_lines), \
+        COMBO("dmax", "Maximum possible delay", 0, art_delay_maxlen), \
+        pan("_in", "Input"), \
+        DRY_GAIN(GAIN_AMP_0_DB), \
+        SWITCH("dm", "Dry mute", 0.0f), \
+        WET_GAIN(GAIN_AMP_0_DB), \
+        SWITCH("wm", "Wet mute", 0.0f), \
+        SWITCH("mono", "Mono output", 0.0f), \
+        SWITCH("fb", "Feedback", 0.0f), \
+        OUT_GAIN
+
+    #define ART_DELAY_TEMPO(id) \
+        CONTROL("tempo", "Tempo", U_BPM, art_delay_base_metadata::TEMPO), \
+        SWITCH("sync", "Tempo sync", 0.0f)
+
+    #define ART_DELAY_PROCESSOR(id, pan) \
+        SWITCH("on" #id, "Delay " #id " on", 0.0f), \
+        COMBO("dt" #id, "Delay " #id " tempo", 0, art_delay_tempo), \
+        pan(#id, "Delay " #id), \
+        SWITCH("s" #id, "Delay " #id " solo", 0.0f), \
+        SWITCH("m" #id, "Delay " #id " mute", 0.0f), \
+        SWITCH("ph" #id, "Delay " #id " phase", 0.0f), \
+        COMBO("dr" #id, "Delay " #id " reference", 0, art_delay_references), \
+        CONTROL("drm" #id, "Delay " #id " reference multiplier", U_NONE, art_delay_base_metadata::DELAY_MULT), \
+        CONTROL("dt" #id, "Delay " #id " time", U_SEC, art_delay_base_metadata::TIME), \
+        CONTROL("dbf" #id, "Delay " #id " bar fraction", U_BAR, art_delay_base_metadata::FRACTION), \
+        INT_CONTROL("dbd" #id, "Delay " #id " bar denominator", U_BEAT, art_delay_base_metadata::DENOMINATOR), \
+        INT_CONTROL("dbm" #id, "Delay " #id " bar multiplier", U_NONE, art_delay_base_metadata::BAR_MULT), \
+        CONTROL("df" #id, "Delay " #id " fraction", U_BAR, art_delay_base_metadata::FRACTION), \
+        INT_CONTROL("dd" #id, "Delay " #id " denominator", U_BEAT, art_delay_base_metadata::DENOMINATOR), \
+        SWITCH("eq" #id, "Equalizer " #id " on", 0.0f), \
+        SWITCH("lfc" #id, "Delay " #id " low-cut", 0.0f), \
+        LOG_CONTROL("flc" #id, "Delay " #id " low-cut frequency", U_HZ, art_delay_base_metadata::LOW_CUT), \
+        SWITCH("hfc" #id, "Delay " #id " high-cut", 0.0f), \
+        LOG_CONTROL("fhc" #id, "Delay " #id " high-cut frequency", U_HZ, art_delay_base_metadata::HIGH_CUT), \
+        LOG_CONTROL("fbs" #id, "Delay " #id " sub-bass", U_GAIN_AMP, art_delay_base_metadata::BAND_GAIN), \
+        LOG_CONTROL("fbb" #id, "Delay " #id " bass", U_GAIN_AMP, art_delay_base_metadata::BAND_GAIN), \
+        LOG_CONTROL("fbm" #id, "Delay " #id " middle", U_GAIN_AMP, art_delay_base_metadata::BAND_GAIN), \
+        LOG_CONTROL("fbp" #id, "Delay " #id " presence", U_GAIN_AMP, art_delay_base_metadata::BAND_GAIN), \
+        LOG_CONTROL("fbt" #id, "Delay " #id " treble", U_GAIN_AMP, art_delay_base_metadata::BAND_GAIN), \
+        AMP_GAIN1("fb" #id, "Feedback " #id, GAIN_AMP_M_INF_DB), \
+        AMP_GAIN10("dg" #id, "Delay " #id " gain", GAIN_AMP_0_DB), \
+        METER("adt" #id, "Delay " #id " actual time", U_SEC, art_delay_base_metadata::TIME), \
+        BLINK("oor" #id, "Delay " #id " out of range"), \
+        BLINK("loop" #id, "Delay " #id " dependency loop")
+
+    #define ART_DELAY_TEMPOS \
+        ART_DELAY_TEMPO(0), \
+        ART_DELAY_TEMPO(1), \
+        ART_DELAY_TEMPO(2), \
+        ART_DELAY_TEMPO(3), \
+        ART_DELAY_TEMPO(4), \
+        ART_DELAY_TEMPO(5), \
+        ART_DELAY_TEMPO(6), \
+        ART_DELAY_TEMPO(7)
+
+    #define ART_DELAY_PROCESSORS(pan) \
+        ART_DELAY_PROCESSOR(0, pan), \
+        ART_DELAY_PROCESSOR(1, pan), \
+        ART_DELAY_PROCESSOR(2, pan), \
+        ART_DELAY_PROCESSOR(3, pan), \
+        ART_DELAY_PROCESSOR(4, pan), \
+        ART_DELAY_PROCESSOR(5, pan), \
+        ART_DELAY_PROCESSOR(6, pan), \
+        ART_DELAY_PROCESSOR(7, pan), \
+        ART_DELAY_PROCESSOR(8, pan), \
+        ART_DELAY_PROCESSOR(9, pan), \
+        ART_DELAY_PROCESSOR(10, pan), \
+        ART_DELAY_PROCESSOR(11, pan), \
+        ART_DELAY_PROCESSOR(12, pan), \
+        ART_DELAY_PROCESSOR(13, pan), \
+        ART_DELAY_PROCESSOR(14, pan), \
+        ART_DELAY_PROCESSOR(15, pan)
+
+    static const port_t art_delay_mono_ports[] =
+    {
+        // Input audio ports
+        AUDIO_INPUT_MONO,
+        AUDIO_OUTPUT_LEFT,
+        AUDIO_OUTPUT_RIGHT,
+        ART_DELAY_COMMON(ART_PAN_MONO),
+        ART_DELAY_TEMPOS,
+        ART_DELAY_PROCESSORS(ART_PAN_MONO),
+
+        PORTS_END
+    };
+
+    static const port_t art_delay_stereo_ports[] =
+    {
+        // Input audio ports
+        PORTS_STEREO_PLUGIN,
+        ART_DELAY_COMMON(ART_PAN_STEREO),
+        ART_DELAY_TEMPOS,
+        ART_DELAY_PROCESSORS(ART_PAN_STEREO),
+
+        PORTS_END
+    };
+
+    static const int art_delay_classes[] = { C_DELAY, -1 };
+
+    const plugin_metadata_t  art_delay_mono_metadata::metadata =
+    {
+        "Künstlerische Verzögerung",
+        "Artistic Delay Mono",
+        "KV16M",
+        &developers::v_sadovnikov,
+        "art_delay_mono",
+        "----",
+        LSP_ART_DELAY_BASE + 0,
+        LSP_VERSION(1, 0, 0),
+        art_delay_classes,
+        E_DUMP_STATE,
+        art_delay_mono_ports,
+        "delay/art_delay/mono.xml",
+        NULL,
+        mono_to_stereo_plugin_port_groups
+    };
+
+    const plugin_metadata_t  art_delay_stereo_metadata::metadata =
+    {
+        "Künstlerische Verzögerung",
+        "Artistic Delay Stereo",
+        "KV16S",
+        &developers::v_sadovnikov,
+        "art_delay_stereo",
+        "----",
+        LSP_ART_DELAY_BASE + 1,
+        LSP_VERSION(1, 0, 0),
+        art_delay_classes,
+        E_DUMP_STATE,
+        art_delay_stereo_ports,
+        "delay/art_delay/stereo.xml",
+        NULL,
+        stereo_plugin_port_groups
+    };
+}
+
+
