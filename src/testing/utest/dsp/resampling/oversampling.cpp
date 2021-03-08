98e3466918b61b11636d59c64fd41ea63821e88e
sadko4u <sadko4u@gmail.com>
2021-03-08 16:40:32 +0300

    * Implemented 4-lobe Lanczos oversampling DSP functions for 32-bit ARM architecture.

--- src/testing/utest/dsp/resampling/oversampling.cpp
+++ src/testing/utest/dsp/resampling/oversampling.cpp
@@ -87,19 +87,19 @@ IF_ARCH_ARM(
     {
         void lanczos_resample_2x2(float *dst, const float *src, size_t count);
         void lanczos_resample_2x3(float *dst, const float *src, size_t count);
-//        void lanczos_resample_2x4(float *dst, const float *src, size_t count);
+        void lanczos_resample_2x4(float *dst, const float *src, size_t count);
         void lanczos_resample_3x2(float *dst, const float *src, size_t count);
         void lanczos_resample_3x3(float *dst, const float *src, size_t count);
-//        void lanczos_resample_3x4(float *dst, const float *src, size_t count);
+        void lanczos_resample_3x4(float *dst, const float *src, size_t count);
         void lanczos_resample_4x2(float *dst, const float *src, size_t count);
         void lanczos_resample_4x3(float *dst, const float *src, size_t count);
-//        void lanczos_resample_4x4(float *dst, const float *src, size_t count);
+        void lanczos_resample_4x4(float *dst, const float *src, size_t count);
         void lanczos_resample_6x2(float *dst, const float *src, size_t count);
         void lanczos_resample_6x3(float *dst, const float *src, size_t count);
-//        void lanczos_resample_6x4(float *dst, const float *src, size_t count);
+        void lanczos_resample_6x4(float *dst, const float *src, size_t count);
         void lanczos_resample_8x2(float *dst, const float *src, size_t count);
         void lanczos_resample_8x3(float *dst, const float *src, size_t count);
-//        void lanczos_resample_8x4(float *dst, const float *src, size_t count);
+        void lanczos_resample_8x4(float *dst, const float *src, size_t count);
     }
 )
 
@@ -212,19 +212,19 @@ UTEST_BEGIN("dsp.resampling", oversampling)
 
         IF_ARCH_ARM(CALL(native::lanczos_resample_2x2, neon_d32::lanczos_resample_2x2, 16, 2));
         IF_ARCH_ARM(CALL(native::lanczos_resample_2x3, neon_d32::lanczos_resample_2x3, 16, 2));
-//        IF_ARCH_ARM(CALL(native::lanczos_resample_2x4, neon_d32::lanczos_resample_2x4, 16, 2));
+        IF_ARCH_ARM(CALL(native::lanczos_resample_2x4, neon_d32::lanczos_resample_2x4, 16, 2));
         IF_ARCH_ARM(CALL(native::lanczos_resample_3x2, neon_d32::lanczos_resample_3x2, 16, 3));
         IF_ARCH_ARM(CALL(native::lanczos_resample_3x3, neon_d32::lanczos_resample_3x3, 16, 3));
-//        IF_ARCH_ARM(CALL(native::lanczos_resample_3x4, neon_d32::lanczos_resample_3x4, 16, 3));
+        IF_ARCH_ARM(CALL(native::lanczos_resample_3x4, neon_d32::lanczos_resample_3x4, 16, 3));
         IF_ARCH_ARM(CALL(native::lanczos_resample_4x2, neon_d32::lanczos_resample_4x2, 16, 4));
         IF_ARCH_ARM(CALL(native::lanczos_resample_4x3, neon_d32::lanczos_resample_4x3, 16, 4));
-//        IF_ARCH_ARM(CALL(native::lanczos_resample_4x4, neon_d32::lanczos_resample_4x4, 16, 4));
+        IF_ARCH_ARM(CALL(native::lanczos_resample_4x4, neon_d32::lanczos_resample_4x4, 16, 4));
         IF_ARCH_ARM(CALL(native::lanczos_resample_6x2, neon_d32::lanczos_resample_6x2, 16, 6));
         IF_ARCH_ARM(CALL(native::lanczos_resample_6x3, neon_d32::lanczos_resample_6x3, 16, 6));
-//        IF_ARCH_ARM(CALL(native::lanczos_resample_6x4, neon_d32::lanczos_resample_6x4, 16, 6));
+        IF_ARCH_ARM(CALL(native::lanczos_resample_6x4, neon_d32::lanczos_resample_6x4, 16, 6));
         IF_ARCH_ARM(CALL(native::lanczos_resample_8x2, neon_d32::lanczos_resample_8x2, 16, 8));
         IF_ARCH_ARM(CALL(native::lanczos_resample_8x3, neon_d32::lanczos_resample_8x3, 16, 8));
-//        IF_ARCH_ARM(CALL(native::lanczos_resample_8x4, neon_d32::lanczos_resample_8x4, 16, 8));
+        IF_ARCH_ARM(CALL(native::lanczos_resample_8x4, neon_d32::lanczos_resample_8x4, 16, 8));
 
         IF_ARCH_AARCH64(CALL(native::lanczos_resample_2x2, asimd::lanczos_resample_2x2, 16, 2));
         IF_ARCH_AARCH64(CALL(native::lanczos_resample_2x3, asimd::lanczos_resample_2x3, 16, 2));
