55c5ce8b1dca40b6b3ab74d76410218369fbb33c
sadko4u <sadko4u@gmail.com>
2022-09-06 00:07:54 +0300

    Updated dependencies

--- modules.mk
+++ modules.mk
@@ -19,92 +19,204 @@
 #
 
 # Variables that describe dependencies
-LSP_3RD_PARTY_VERSION      := 1.0.2
+LSP_3RD_PARTY_VERSION      := 1.0.3
 LSP_3RD_PARTY_NAME         := lsp-3rd-party
 LSP_3RD_PARTY_TYPE         := hdr
 LSP_3RD_PARTY_URL_RO       := https://github.com/lsp-plugins/$(LSP_3RD_PARTY_NAME).git
 LSP_3RD_PARTY_URL_RW       := git@github.com:lsp-plugins/$(LSP_3RD_PARTY_NAME).git
 
-LSP_COMMON_LIB_VERSION     := 1.0.19
+LSP_COMMON_LIB_VERSION     := 1.0.22
 LSP_COMMON_LIB_NAME        := lsp-common-lib
 LSP_COMMON_LIB_TYPE        := src
 LSP_COMMON_LIB_URL_RO      := https://github.com/lsp-plugins/$(LSP_COMMON_LIB_NAME).git
 LSP_COMMON_LIB_URL_RW      := git@github.com:lsp-plugins/$(LSP_COMMON_LIB_NAME).git
 
-LSP_DSP_LIB_VERSION        := 1.0.3
+LSP_DSP_LIB_VERSION        := 1.0.6
 LSP_DSP_LIB_NAME           := lsp-dsp-lib
 LSP_DSP_LIB_TYPE           := src
 LSP_DSP_LIB_URL_RO         := https://github.com/lsp-plugins/$(LSP_DSP_LIB_NAME).git
 LSP_DSP_LIB_URL_RW         := git@github.com:lsp-plugins/$(LSP_DSP_LIB_NAME).git
 
-LSP_DSP_UNITS_VERSION      := 1.0.2
+LSP_DSP_UNITS_VERSION      := 1.0.5
 LSP_DSP_UNITS_NAME         := lsp-dsp-units
 LSP_DSP_UNITS_TYPE         := src
 LSP_DSP_UNITS_URL_RO       := https://github.com/lsp-plugins/$(LSP_DSP_UNITS_NAME).git
 LSP_DSP_UNITS_URL_RW       := git@github.com:lsp-plugins/$(LSP_DSP_UNITS_NAME).git
 
-LSP_LLTL_LIB_VERSION       := 1.0.3
+LSP_LLTL_LIB_VERSION       := 1.0.6
 LSP_LLTL_LIB_NAME          := lsp-lltl-lib
 LSP_LLTL_LIB_TYPE          := src
 LSP_LLTL_LIB_URL_RO        := https://github.com/lsp-plugins/$(LSP_LLTL_LIB_NAME).git
 LSP_LLTL_LIB_URL_RW        := git@github.com:lsp-plugins/$(LSP_LLTL_LIB_NAME).git
 
-LSP_PLUGIN_FW_VERSION      := 1.0.4
+LSP_PLUGIN_FW_VERSION      := 1.0.5
 LSP_PLUGIN_FW_NAME         := lsp-plugin-fw
 LSP_PLUGIN_FW_TYPE         := src
 LSP_PLUGIN_FW_URL_RO       := https://github.com/lsp-plugins/$(LSP_PLUGIN_FW_NAME).git
 LSP_PLUGIN_FW_URL_RW       := git@github.com:lsp-plugins/$(LSP_PLUGIN_FW_NAME).git
 
-LSP_PLUGINS_SHARED_VERSION := 1.0.3
-LSP_PLUGINS_SHARED_NAME    := lsp-plugins-shared
-LSP_PLUGINS_SHARED_TYPE    := src
-LSP_PLUGINS_SHARED_URL_RO  := https://github.com/lsp-plugins/$(LSP_PLUGINS_SHARED_NAME).git
-LSP_PLUGINS_SHARED_URL_RW  := git@github.com:lsp-plugins/$(LSP_PLUGINS_SHARED_NAME).git
-
-LSP_R3D_BASE_LIB_VERSION   := 1.0.3
+LSP_R3D_BASE_LIB_VERSION   := 1.0.6
 LSP_R3D_BASE_LIB_NAME      := lsp-r3d-base-lib
 LSP_R3D_BASE_LIB_TYPE      := src
 LSP_R3D_BASE_LIB_URL_RO    := https://github.com/lsp-plugins/$(LSP_R3D_BASE_LIB_NAME).git
 LSP_R3D_BASE_LIB_URL_RW    := git@github.com:lsp-plugins/$(LSP_R3D_BASE_LIB_NAME).git
 
-LSP_R3D_IFACE_VERSION      := 1.0.3
+LSP_R3D_IFACE_VERSION      := 1.0.6
 LSP_R3D_IFACE_NAME         := lsp-r3d-iface
 LSP_R3D_IFACE_TYPE         := src
 LSP_R3D_IFACE_URL_RO       := https://github.com/lsp-plugins/$(LSP_R3D_IFACE_NAME).git
 LSP_R3D_IFACE_URL_RW       := git@github.com:lsp-plugins/$(LSP_R3D_IFACE_NAME).git
 
-LSP_R3D_GLX_LIB_VERSION    := 1.0.3
+LSP_R3D_GLX_LIB_VERSION    := 1.0.6
 LSP_R3D_GLX_LIB_NAME       := lsp-r3d-glx-lib
 LSP_R3D_GLX_LIB_TYPE       := bin
 LSP_R3D_GLX_LIB_URL_RO     := https://github.com/lsp-plugins/$(LSP_R3D_GLX_LIB_NAME).git
 LSP_R3D_GLX_LIB_URL_RW     := git@github.com:lsp-plugins/$(LSP_R3D_GLX_LIB_NAME).git
 
-LSP_RUNTIME_LIB_VERSION    := 1.0.5
+LSP_R3D_WGL_LIB_VERSION    := 1.0.1
+LSP_R3D_WGL_LIB_NAME       := lsp-r3d-wgl-lib
+LSP_R3D_WGL_LIB_TYPE       := bin
+LSP_R3D_WGL_LIB_URL_RO     := https://github.com/lsp-plugins/$(LSP_R3D_WGL_LIB_NAME).git
+LSP_R3D_WGL_LIB_URL_RW     := git@github.com:lsp-plugins/$(LSP_R3D_WGL_LIB_NAME).git
+
+LSP_RUNTIME_LIB_VERSION    := 1.0.9
 LSP_RUNTIME_LIB_NAME       := lsp-runtime-lib
 LSP_RUNTIME_LIB_TYPE       := src
 LSP_RUNTIME_LIB_URL_RO     := https://github.com/lsp-plugins/$(LSP_RUNTIME_LIB_NAME).git
 LSP_RUNTIME_LIB_URL_RW     := git@github.com:lsp-plugins/$(LSP_RUNTIME_LIB_NAME).git
 
-LSP_WS_LIB_VERSION         := 1.0.3
-LSP_WS_LIB_NAME            := lsp-ws-lib
-LSP_WS_LIB_TYPE            := src
-LSP_WS_LIB_URL_RO          := https://github.com/lsp-plugins/$(LSP_WS_LIB_NAME).git
-LSP_WS_LIB_URL_RW          := git@github.com:lsp-plugins/$(LSP_WS_LIB_NAME).git
-
-LSP_TEST_FW_VERSION        := 1.0.13
+LSP_TEST_FW_VERSION        := 1.0.16
 LSP_TEST_FW_NAME           := lsp-test-fw
 LSP_TEST_FW_TYPE           := src
 LSP_TEST_FW_URL_RO         := https://github.com/lsp-plugins/$(LSP_TEST_FW_NAME).git
 LSP_TEST_FW_URL_RW         := git@github.com:lsp-plugins/$(LSP_TEST_FW_NAME).git
 
-LSP_TK_LIB_VERSION         := 1.0.3
+LSP_TK_LIB_VERSION         := 1.0.5
 LSP_TK_LIB_NAME            := lsp-tk-lib
 LSP_TK_LIB_TYPE            := src
 LSP_TK_LIB_URL_RO          := https://github.com/lsp-plugins/$(LSP_TK_LIB_NAME).git
 LSP_TK_LIB_URL_RW          := git@github.com:lsp-plugins/$(LSP_TK_LIB_NAME).git
 
-# Specify description of plugin dependencies
+LSP_WS_LIB_VERSION         := 1.0.5
+LSP_WS_LIB_NAME            := lsp-ws-lib
+LSP_WS_LIB_TYPE            := src
+LSP_WS_LIB_URL_RO          := https://github.com/lsp-plugins/$(LSP_WS_LIB_NAME).git
+LSP_WS_LIB_URL_RW          := git@github.com:lsp-plugins/$(LSP_WS_LIB_NAME).git
 
+LSP_PLUGINS_SHARED_VERSION := 1.0.4
+LSP_PLUGINS_SHARED_NAME    := lsp-plugins-shared
+LSP_PLUGINS_SHARED_TYPE    := src
+LSP_PLUGINS_SHARED_URL_RO  := https://github.com/lsp-plugins/$(LSP_PLUGINS_SHARED_NAME).git
+LSP_PLUGINS_SHARED_URL_RW  := git@github.com:lsp-plugins/$(LSP_PLUGINS_SHARED_NAME).git
+
+# System libraries
+LIBCAIRO_VERSION           := system
+LIBCAIRO_NAME              := cairo
+LIBCAIRO_TYPE              := pkg
+
+LIBDL_VERSION              := system
+LIBDL_NAME                 := libdl
+LIBDL_TYPE                 := opt
+LIBDL_LDFLAGS              := -ldl
+
+LIBD2D1_VERSION            := system
+LIBD2D1_NAME               := libd2d1
+LIBD2D1_TYPE               := opt
+LIBD2D1_LDFLAGS            := -ld2d1
+
+LIBDWRITE_VERSION          := system
+LIBDWRITE_NAME             := libdwrite
+LIBDWRITE_TYPE             := opt
+LIBDWRITE_LDFLAGS          := -ldwrite
+
+LIBFREETYPE_VERSION        := system
+LIBFREETYPE_NAME           := freetype2
+LIBFREETYPE_TYPE           := pkg
+
+LIBGDI32_VERSION           := system
+LIBGDI32_NAME              := libgid32
+LIBGDI32_TYPE              := opt
+LIBGDI32_LDFLAGS           := -lgdi32
+
+LIBGL_VERSION              := system
+LIBGL_NAME                 := gl
+LIBGL_TYPE                 := pkg
+
+LIBICONV_VERSION           := system
+LIBICONV_NAME              := libiconv
+LIBICONV_TYPE              := opt
+LIBICONV_LDFLAGS           := -liconv
+
+LIBMPR_VERSION             := system
+LIBMPR_NAME                := libmpr
+LIBMPR_TYPE                := opt
+LIBMPR_LDFLAGS             := -lmpr
+
+LIBJACK_VERSION            := system
+LIBJACK_NAME               := jack
+LIBJACK_TYPE               := pkg
+
+LIBLADSPA_VERSION          := system
+LIBLADSPA_NAME             := ladspa
+LIBLADSPA_TYPE             := opt
+
+LIBLV2_VERSION             := system
+LIBLV2_NAME                := lv2
+LIBLV2_TYPE                := pkg
+
+LIBMSACM_VERSION           := system
+LIBMSACM_NAME              := libmsacm
+LIBMSACM_TYPE              := opt
+LIBMSACM_LDFLAGS           := -lmsacm32
+
+LIBOLE_VERSION             := system
+LIBOLE_NAME                := libole
+LIBOLE_TYPE                := opt
+LIBOLE_LDFLAGS             := -lole32
+
+LIBOPENGL32_VERSION        := system
+LIBOPENGL32_NAME           := libole
+LIBOPENGL32_TYPE           := opt
+LIBOPENGL32_LDFLAGS        := -lopengl32
+
+LIBPTHREAD_VERSION         := system
+LIBPTHREAD_NAME            := libpthread
+LIBPTHREAD_TYPE            := opt
+LIBPTHREAD_LDFLAGS         := -lpthread
+
+LIBSNDFILE_VERSION         := system
+LIBSNDFILE_NAME            := sndfile
+LIBSNDFILE_TYPE            := pkg
+
+LIBSHLWAPI_VERSION         := system
+LIBSHLWAPI_NAME            := libshlwapi
+LIBSHLWAPI_TYPE            := opt
+LIBSHLWAPI_LDFLAGS         := -lshlwapi
+
+LIBUUID_VERSION            := system
+LIBUUID_NAME               := libuuid
+LIBUUID_TYPE               := opt
+LIBUUID_LDFLAGS            := -luuid
+
+LIBWINCODEC_VERSION        := system
+LIBWINCODEC_NAME           := libwincodec
+LIBWINCODEC_TYPE           := opt
+LIBWINCODEC_LDFLAGS        := -lwindowscodecs
+
+LIBWINMM_VERSION           := system
+LIBWINMM_NAME              := libwinmm
+LIBWINMM_TYPE              := opt
+LIBWINMM_LDFLAGS           := -lwinmm
+
+LIBX11_VERSION             := system
+LIBX11_NAME                := x11
+LIBX11_TYPE                := pkg
+
+LIBXRANDR_VERSION          := system
+LIBXRANDR_NAME             := xrandr
+LIBXRANDR_TYPE             := pkg
+
+# Specify description of plugin dependencies
 LSP_PLUGINS_ART_DELAY_VERSION           := 1.0.4
 LSP_PLUGINS_ART_DELAY_NAME              := lsp-plugins-art-delay
 LSP_PLUGINS_ART_DELAY_TYPE              := plug
@@ -273,61 +385,4 @@ LSP_PLUGINS_TRIGGER_TYPE                := plug
 LSP_PLUGINS_TRIGGER_URL_RO              := https://github.com/lsp-plugins/$(LSP_PLUGINS_TRIGGER_NAME).git
 LSP_PLUGINS_TRIGGER_URL_RW              := git@github.com:lsp-plugins/$(LSP_PLUGINS_TRIGGER_NAME).git
 
-# Specify description of library dependencies
-LIBSNDFILE_VERSION         := system
-LIBSNDFILE_NAME            := sndfile
-LIBSNDFILE_TYPE            := pkg
 
-LIBJACK_VERSION            := system
-LIBJACK_NAME               := jack
-LIBJACK_TYPE               := pkg
-
-LIBX11_VERSION             := system
-LIBX11_NAME                := x11
-LIBX11_TYPE                := pkg
-
-LIBXRANDR_VERSION          := system
-LIBXRANDR_NAME             := xrandr
-LIBXRANDR_TYPE             := pkg
-
-LIBCAIRO_VERSION           := system
-LIBCAIRO_NAME              := cairo
-LIBCAIRO_TYPE              := pkg
-
-LIBGL_VERSION              := system
-LIBGL_NAME                 := gl
-LIBGL_TYPE                 := pkg
-
-LIBPTHREAD_VERSION         := system
-LIBPTHREAD_NAME            := libpthread
-LIBPTHREAD_TYPE            := opt
-LIBPTHREAD_LDFLAGS         := -lpthread
-
-LIBDL_VERSION              := system
-LIBDL_NAME                 := libdl
-LIBDL_TYPE                 := opt
-LIBDL_LDFLAGS              := -ldl
-
-LIBICONV_VERSION           := system
-LIBICONV_NAME              := libiconv
-LIBICONV_TYPE              := opt
-LIBICONV_LDFLAGS           := -liconv
-
-LIBFREETYPE_VERSION        := system
-LIBFREETYPE_NAME           := freetype2
-LIBFREETYPE_TYPE           := pkg
-
-LIBSHLWAPI_VERSION         := system
-LIBSHLWAPI_NAME            := libshlwapi
-LIBSHLWAPI_TYPE            := opt
-LIBSHLWAPI_LDFLAGS         := -lshlwapi
-
-LIBWINMM_VERSION           := system
-LIBWINMM_NAME              := libwinmm
-LIBWINMM_TYPE              := opt
-LIBWINMM_LDFLAGS           := -lwinmm
-
-LIBMSACM_VERSION           := system
-LIBMSACM_NAME              := libmsacm
-LIBMSACM_TYPE              := opt
-LIBMSACM_LDFLAGS           := -lmsacm32
